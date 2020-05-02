3ae96fba7328e081cdfb1f5d8229ce7eb814f7c0
DavAug <david.augustin@gmx.net>
2020-05-02 14:19:40 +0200

    more in views and template rendering

--- polls/urls.py
+++ polls/urls.py
@@ -6,6 +6,7 @@ from django.urls import path
 
 from . import views
 
+app_name = 'polls'
 urlpatterns = [
     # ex: /polls/
     path('', views.index, name='index'),
--- polls/views.py
+++ polls/views.py
@@ -12,19 +12,37 @@
 # webpage more modular and easier to update and maintain.
 #
 
-from django.http import HttpResponse
+from django.http import HttpResponse  # , Http404
+# from django.template import loader  # this is the lengthy version
+from django.shortcuts import get_object_or_404, render
 
 from .models import Question
 
 
+# def index(request):
+#     latest_question_list = Question.objects.order_by('-pub_date')[:5]
+#     template = loader.get_template('polls/index.html')
+#     context = {
+#         'latest_question_list': latest_question_list,
+#     }
+#     return HttpResponse(template.render(context, request))
+
 def index(request):
     latest_question_list = Question.objects.order_by('-pub_date')[:5]
-    output = ', '.join([q.question_text for q in latest_question_list])
-    return HttpResponse(output)
+    context = {'latest_question_list': latest_question_list}
+    return render(request, 'polls/index.html', context)
 
+
+# def detail(request, question_id):
+#     try:
+#         question = Question.objects.get(pk=question_id)
+#     except Question.DoesNotExist:
+#         raise Http404("Question does not exist")
+#     return render(request, 'polls/detail.html', {'question': question})
 
 def detail(request, question_id):
-    return HttpResponse("You're looking at question %s." % question_id)
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, 'polls/detail.html', {'question': question})
 
 
 def results(request, question_id):
