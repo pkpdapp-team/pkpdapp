ab812ebba450a1412bd5fe7187484e5ca157ff7d
DavAug <david.augustin@gmx.net>
2020-05-02 12:21:49 +0200

    more views for polls

--- polls/views.py
+++ polls/views.py
@@ -1,10 +1,36 @@
 #
 # Creates views that can be called by mapping them to urls.
 #
+# Each view is responsible for one of two things: return HttpResponse, or
+# raising exception Http404.
+#
+# All functionality of the of the webpage lies within the views. They can
+# can be used to read records from database, use a template system, or use
+# any other third party python package.
+#
+# NOTE: Always separate functions from design by using templates. This makes
+# webpage more modular and easier to update and maintain.
+#
 
 from django.http import HttpResponse
 
+from .models import Question
+
 
 def index(request):
-    """Defines a particular view, that can be called by mapping it to a url."""
-    return HttpResponse("Hello, world. You're at the polls index.")
+    latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    output = ', '.join([q.question_text for q in latest_question_list])
+    return HttpResponse(output)
+
+
+def detail(request, question_id):
+    return HttpResponse("You're looking at question %s." % question_id)
+
+
+def results(request, question_id):
+    response = "You're looking at the results of question %s."
+    return HttpResponse(response % question_id)
+
+
+def vote(request, question_id):
+    return HttpResponse("You're voting on question %s." % question_id)
