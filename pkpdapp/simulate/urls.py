7f55baf4392b60474c1ca32953bb6267ab600d98
DavAug <david.augustin@gmx.net>
2020-05-25 11:20:58 +0200

    rough model building interface

--- pkpdapp/simulate/urls.py
+++ pkpdapp/simulate/urls.py
@@ -2,6 +2,10 @@ from django.urls import path
 
 from . import views
 
+# namespace for template and view calls form other apps
+app_name = 'simulate'
+
+# url patterns within the `simulate` app
 urlpatterns = [
     path('', views.IndexView.as_view(), name='index'),
 ]
