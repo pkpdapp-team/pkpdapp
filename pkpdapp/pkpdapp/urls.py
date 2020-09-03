76db249b3b616e9238761466fb5cdd656d55f1b1
DavAug <david.augustin@gmx.net>
2020-09-03 09:36:12 +0100

    flake8

--- pkpdapp/pkpdapp/urls.py
+++ pkpdapp/pkpdapp/urls.py
@@ -10,7 +10,7 @@ URL config of the pkpdapp project.
 The `urlpatterns` list routes URLs to views. For more information please see
 https://docs.djangoproject.com/en/3.0/topics/http/urls/.
 """
-from django.contrib import admin
+
 from django.urls import include, path
 
 from . import views
