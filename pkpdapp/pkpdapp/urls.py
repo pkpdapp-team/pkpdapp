e530a1fbc8d63311e12d65c50ffaebeb9a4b376f
DavAug <david.augustin@gmx.net>
2020-09-03 08:10:21 +0100

    some comments

--- pkpdapp/pkpdapp/urls.py
+++ pkpdapp/pkpdapp/urls.py
@@ -15,10 +15,11 @@ from django.urls import include, path
 
 from . import views
 
+
+# TODO: Move django_plotly_dash to the app that is actually using it!
 urlpatterns = [
     path('', views.IndexView.as_view(), name='index'),
     path('generic/', views.GenericView.as_view(), name='generic'),
     path('simulate/', include('simulate.urls')),
     path('django_plotly_dash/', include('django_plotly_dash.urls')),
-    path('admin/', admin.site.urls),
 ]
