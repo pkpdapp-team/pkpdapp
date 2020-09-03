22f04180751fb5cff274193e2fed9945010233c3
DavAug <david.augustin@gmx.net>
2020-09-03 08:10:33 +0100

    clean up views

--- pkpdapp/pkpdapp/views.py
+++ pkpdapp/pkpdapp/views.py
@@ -11,26 +11,12 @@ For more information please see
 https://docs.djangoproject.com/en/3.0/topics/http/views/.
 """
 
-from django.views import generic
+from django.views.generic import TemplateView
 
 
-class IndexView(generic.base.TemplateView):
+class IndexView(TemplateView):
     template_name = 'index.html'
 
-    def get_context_data(self):
-        """
-        Returns the specifications of the web page.
-        """
-        context = {}
-        return context
 
-
-class GenericView(generic.base.TemplateView):
+class GenericView(TemplateView):
     template_name = 'generic.html'
-
-    def get_context_data(self):
-        """
-        Returns the specifications of the web page.
-        """
-        context = {}
-        return context
