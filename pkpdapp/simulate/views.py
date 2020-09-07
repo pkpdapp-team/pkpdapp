f2f7dc41a2d626ee5a41562bc9d77d3950f26879
DavAug <david.augustin@gmx.net>
2020-09-07 20:50:55 +0100

    remove unused method in view

--- pkpdapp/simulate/views.py
+++ pkpdapp/simulate/views.py
@@ -12,10 +12,3 @@ class IndexView(generic.base.TemplateView):
     This class organises the simulate workflow of the PKPDApp.
     """
     template_name = 'simulate/index.html'
-
-    def get_context_data(self):
-        """
-        Returns the specifications of the web page.
-        """
-        context = {}
-        return context
