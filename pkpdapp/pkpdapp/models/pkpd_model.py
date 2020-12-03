d3aa81a7106d708b2556500506b8e599d35300bb
Martin Robinson <martinjrobins@gmail.com>
2020-12-03 10:12:01 +0000

    #54 add copyright notices

--- pkpdapp/pkpdapp/models/pkpd_model.py
+++ pkpdapp/pkpdapp/models/pkpd_model.py
@@ -1,7 +1,16 @@
+#
+# This file is part of PKPDApp (https://github.com/pkpdapp-team/pkpdapp) which
+# is released under the BSD 3-clause license. See accompanying LICENSE.md for
+# copyright notice and full license details.
+#
+
 from django.db import models
 
 
 class PkpdModel(models.Model):
+    """
+    A PK or PD model, represented using SBML
+    """
     MODEL_TYPE_CHOICES = [
         ('PK', 'Pharmokinetic'),
         ('PD', 'Pharmodynamic'),
--- pkpdapp/pkpdapp/tests/test_models.py
+++ pkpdapp/pkpdapp/tests/test_models.py
@@ -1,3 +1,9 @@
+#
+# This file is part of PKPDApp (https://github.com/pkpdapp-team/pkpdapp) which
+# is released under the BSD 3-clause license. See accompanying LICENSE.md for
+# copyright notice and full license details.
+#
+
 from django.test import TestCase
 from pkpdapp.models import (
     Dataset, Project, Biomarker, BiomarkerType, PkpdModel
