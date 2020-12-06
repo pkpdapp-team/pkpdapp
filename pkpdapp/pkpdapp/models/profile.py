a6c10b70497e32c39a4307e40774b034c9381de1
Martin Robinson <martinjrobins@gmail.com>
2020-12-06 08:25:42 +0000

    #39 docstring for Profile

--- pkpdapp/pkpdapp/models/profile.py
+++ pkpdapp/pkpdapp/models/profile.py
@@ -10,6 +10,13 @@ from django.contrib.auth.models import User
 
 
 class Profile(models.Model):
+    """
+    A user profile, containing neccessary information for users not held in
+    :model:`auth.User`
+    """
     user = models.OneToOneField(User, on_delete=models.CASCADE)
-    selected_project = models.ForeignKey(Project,
-                                         null=True, on_delete=models.SET_NULL)
+    selected_project = models.ForeignKey(
+        Project,
+        help_text='currently selected project for user',
+        null=True, on_delete=models.SET_NULL
+    )
