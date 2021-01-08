f966beeacd76f5fa1fdd97924a98e3069315174b
DavAug <david.augustin@gmx.net>
2021-01-08 14:41:56 +0100

    #71 add erlotinib dependency

--- setup.py
+++ setup.py
@@ -34,14 +34,19 @@ setup(
 
     # List of dependencies
     install_requires=[
+        'dash-bootstrap-components>=0.10',
         'Django==3.0.7',
         'django_plotly_dash==1.3.1',
         'dpd-static-support==0.0.5',
+        'erlotinib @ git+git://github.com/DavAug/erlotinib.git#egg=erlotinib',
         'myokit>=1.31',
         'numpy>=1.8',
         'pandas==1.0.3',
         'pints>=0.3',
         'plotly==4.8.1',
         'whitenoise==5.0.1',
+    ],
+    dependency_links=[
+        "git+git://github.com/DavAug/erlotinib.git#egg=erlotinib-latest",
     ],
 )
