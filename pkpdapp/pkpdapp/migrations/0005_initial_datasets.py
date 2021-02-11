cb4ee1f02a5035a7a560aacf34403198c50b11fa
Martin Robinson <martinjrobins@gmail.com>
2021-02-11 16:23:05 +0000

    #94 add some more models and datasets

--- pkpdapp/pkpdapp/migrations/0005_initial_datasets.py
+++ pkpdapp/pkpdapp/migrations/0005_initial_datasets.py
@@ -11,9 +11,16 @@ import codecs
 from datetime import datetime
 from django.utils.timezone import make_aware
 
-datafile_url = 'https://raw.githubusercontent.com/pkpdapp-team/pkpdapp-datafiles/main/datasets/lxf_control_growth.csv'  # noqa: E501
+datafile_urls = [
+    'https://raw.githubusercontent.com/pkpdapp-team/pkpdapp-datafiles/main/datasets/lxf_control_growth.csv',  # noqa: E501
+    'https://raw.githubusercontent.com/pkpdapp-team/pkpdapp-datafiles/main/datasets/lxf_high_erlotinib_dose.csv',  # noqa: E501
+    'https://raw.githubusercontent.com/pkpdapp-team/pkpdapp-datafiles/main/datasets/lxf_low_erlotinib_dose.csv',  # noqa: E501
+    'https://raw.githubusercontent.com/pkpdapp-team/pkpdapp-datafiles/main/datasets/lxf_medium_erlotinib_dose.csv',  # noqa: E501
+    'https://raw.githubusercontent.com/pkpdapp-team/pkpdapp-datafiles/main/datasets/lxf_single_erlotinib_dose.csv',  # noqa: E501
+]
 
-datafile_description = '''
+datafile_descriptions = [
+'''
 The dataset from [1] contains the time series data of 8 mice with
 patient-derived lung cancer implants. The tumour volume of each
 mouse was monitored over a period of 30 days and measured a
@@ -24,17 +31,126 @@ References
 .. [1] Eigenmann, M. J. et al., Combining Nonclinical Experiments with
     Translational PKPD Modeling to Differentiate Erlotinib and
     Gefitinib, Mol Cancer Ther. 2016; 15(12):3110-3119.
-'''
+''',
+"""
+Returns the high erlotinib dose lung cancer treatment group data
+published in [1]_ as a :class:`pandas.DataFrame`.
 
-biomarkers = [
-    {
-        'name': 'Tumour volume',
-        'unit': 'cm^3',
-    },
-    {
-        'name': 'Body weight',
-        'unit': 'g',
-    },
+The dataset contains the time series data of 6 mice with
+patient-derived lung cancer implants. Each mouse was treated with
+an oral dose of erlotinib of :math:`100\, \text{mg}` per
+:math:`\text{g}` body weight. The dose was administered daily from
+day 3 to day 16, with a treatment break on days 9 to 13.
+
+The blood plasma concentration of erlotinib was measured on day 14,
+while the tumour volume of each mouse was monitored over a period
+of 30 days and measured a couple times a week.
+""",
+"""
+Returns the low erlotinib dose lung cancer treatment group data
+published in [1]_ as a :class:`pandas.DataFrame`.
+
+The dataset contains the time series data of 8 mice with
+patient-derived lung cancer implants. Each mouse was treated with
+an oral dose of erlotinib of :math:`6.25\, \text{mg}` per
+:math:`\text{g}` body weight. The dose was administered daily from
+day 3 to day 16.
+
+The blood plasma concentration of erlotinib was measured on day 10 and 16, while the tumour volume of each mouse was monitored over a period of 30 days and measured a couple times a week.
+""",
+"""
+Returns the medium erlotinib dose lung cancer treatment group data
+published in [1]_ as a :class:`pandas.DataFrame`.
+
+The dataset contains the time series data of 8 mice with
+patient-derived lung cancer implants. Each mouse was treated with
+an oral dose of erlotinib of :math:`25\, \text{mg}` per
+:math:`\text{g}` body weight. The dose was administered daily from
+day 3 to day 16.
+
+The blood plasma concentration of erlotinib was measured on day 10 and 16, while the tumour volume of each mouse was monitored over a period of 30 days and measured a couple times a week.
+""",
+"""
+Returns the single erlotinib dose lung cancer treatment group data
+published in [1]_ as a :class:`pandas.DataFrame`.
+
+The dataset contains the time series data of 30 mice with
+patient-derived lung cancer implants. Each mouse was treated with
+a single oral dose of erlotinib of :math:`100\, \text{mg}` per
+:math:`\text{g}` body weight. The dose was administered either on
+day 0 or day 4.
+
+The blood plasma concentration of erlotinib was measured only once per mouse, either on day 0 or day 4.
+""",
+]
+
+biomarkers_for_datasets = [
+    [
+        {
+            'name': 'Tumour volume',
+            'unit': 'cm^3',
+        },
+        {
+            'name': 'Body weight',
+            'unit': 'g',
+        },
+    ],
+    [
+        {
+            'name': 'Tumour volume',
+            'unit': 'cm^3',
+        },
+        {
+            'name': 'Body weight',
+            'unit': 'g',
+        },
+        {
+            'name': 'Plasma concentration',
+            'unit': 'ng/mL',
+        },
+    ],
+    [
+        {
+            'name': 'Tumour volume',
+            'unit': 'cm^3',
+        },
+        {
+            'name': 'Body weight',
+            'unit': 'g',
+        },
+        {
+            'name': 'Plasma concentration',
+            'unit': 'ng/mL',
+        },
+    ],
+    [
+        {
+            'name': 'Tumour volume',
+            'unit': 'cm^3',
+        },
+        {
+            'name': 'Body weight',
+            'unit': 'g',
+        },
+        {
+            'name': 'Plasma concentration',
+            'unit': 'ng/mL',
+        },
+    ],
+    [
+        {
+            'name': 'Tumour volume',
+            'unit': 'cm^3',
+        },
+        {
+            'name': 'Body weight',
+            'unit': 'g',
+        },
+        {
+            'name': 'Plasma concentration',
+            'unit': 'ng/mL',
+        },
+    ],
 ]
 
 
@@ -44,59 +160,67 @@ def load_datasets(apps, schema_editor):
     BiomarkerType = apps.get_model("pkpdapp", "BiomarkerType")
     Project = apps.get_model("pkpdapp", "Project")
 
-    # create the dataset
-    dataset = Dataset(
-        name='lung_cancer_control_group',
-        description=datafile_description,
-        datetime=make_aware(datetime.today()),
-        administration_type='type1',
-    )
-    dataset.save()
-
-    # add to demo project
-    demo_project = Project.objects.get(name='demo')
-    demo_project.datasets.add(dataset)
-
-    # find the index of the biomarker type, so we don't have to keep looking it
-    # up
-    biomarker_index = {}
-    for i, b in enumerate(biomarkers):
-        biomarker_index[b['name']] = i
-
-    # create all the biomarker types for that dataset
-    biomarker_types = [
-        BiomarkerType(
-            name=b['name'],
-            description=b['name'],
-            unit=b['unit'],
-            dataset=dataset
+    for datafile_url, datafile_description, biomarkers \
+            in zip(datafile_urls, datafile_descriptions,
+                   biomarkers_for_datasets):
+        # create the dataset
+        dataset = Dataset(
+            name='lung_cancer_control_group',
+            description=datafile_description,
+            datetime=make_aware(datetime.today()),
+            administration_type='type1',
         )
-        for b in biomarkers
-    ]
-    [bm.save() for bm in biomarker_types]
+        dataset.save()
 
-    # create all the biomarker measurements for that dataset
-    with urllib.request.urlopen(datafile_url) as f:
-        # parse as csv file
-        data_reader = csv.reader(codecs.iterdecode(f, 'utf-8'))
+        # add to demo project
+        demo_project = Project.objects.get(name='demo')
+        demo_project.datasets.add(dataset)
 
-        # skip the header
-        next(data_reader)
+        # find the index of the biomarker type, so we don't have to keep looking it
+        # up
+        biomarker_index = {}
+        for i, b in enumerate(biomarkers):
+            biomarker_index[b['name']] = i
 
-        # create entries
-        TIME_COLUMN = 1
-        VALUE_COLUMN = 4
-        BIOMARKER_TYPE_COLUMN = 3
-        SUBJECT_ID_COLUMN = 0
-        for row in data_reader:
-            index = biomarker_index[row[BIOMARKER_TYPE_COLUMN]]
-            biomarker = Biomarker(
-                time=row[TIME_COLUMN],
-                subject_id=row[SUBJECT_ID_COLUMN],
-                value=row[VALUE_COLUMN],
-                biomarker_type=biomarker_types[index]
+        # create all the biomarker types for that dataset
+        biomarker_types = [
+            BiomarkerType(
+                name=b['name'],
+                description=b['name'],
+                unit=b['unit'],
+                dataset=dataset
             )
-            biomarker.save()
+            for b in biomarkers
+        ]
+        [bm.save() for bm in biomarker_types]
+
+        # create all the biomarker measurements for that dataset
+        with urllib.request.urlopen(datafile_url) as f:
+            # parse as csv file
+            data_reader = csv.reader(codecs.iterdecode(f, 'utf-8'))
+
+            # skip the header
+            next(data_reader)
+
+            # create entries
+            TIME_COLUMN = 1
+            VALUE_COLUMN = 4
+            BIOMARKER_TYPE_COLUMN = 3
+            SUBJECT_ID_COLUMN = 0
+            for row in data_reader:
+                biomarker_type_str = row[BIOMARKER_TYPE_COLUMN]
+                if not biomarker_type_str:
+                    continue
+                index = biomarker_index[biomarker_type_str]
+                value = row[VALUE_COLUMN]
+                if value.isnumeric():
+                    biomarker = Biomarker(
+                        time=row[TIME_COLUMN],
+                        subject_id=row[SUBJECT_ID_COLUMN],
+                        value=value,
+                        biomarker_type=biomarker_types[index]
+                    )
+                    biomarker.save()
 
 
 def delete_datasets(apps, schema_editor):
--- pkpdapp/pkpdapp/migrations/0006_initial_pkpd_models.py
+++ pkpdapp/pkpdapp/migrations/0006_initial_pkpd_models.py
@@ -49,6 +49,72 @@ $$
             'model_type': 'Pharmacodynamic',
             'sbml_url':
             'https://raw.githubusercontent.com/pkpdapp-team/pkpdapp-datafiles/main/models/tgi_Koch_2009.xml'  # noqa: E501
+        },
+        {
+            'name':
+            'tumour_growth_inhibition_model_koch_reparametrised',
+            'description': """
+Returns the absolute path to a SBML file, specifying the tumour growth
+inhibition pharmacodynamic model introduced by Koch et al. in [1]_ with
+modified parametrisation.
+
+In this model the tumour growth inhibition is modelled by an empirical
+model of the tumour volume :math:`V_T` over time
+
+.. math::
+    \frac{\text{d}V_T}{\text{d}t} =
+        \frac{\lambda V_T}
+        {V_T / V_{\text{crit}} + 1} - \kappa C V_T.
+
+
+Here, the tumour growth in absence of the drug is assumed to grow
+exponentially at rate :math:`\lambda` for tumour volumes below some
+critical volume :math:`V_{\text{crit}}`. For volumes beyond
+:math:`V_{\text{crit}}` the growth dynamics is assumed to slow down
+and transition to a linear growth at rate
+:math:`\lambda V_{\text{crit}}`. The tumour growth inhibitory effect
+of the compound is modelled proportionally to its concentration
+:math:`C` and the current tumour volume. The proportionality factor
+:math:`\kappa` can be interpreted as the potency of the drug.
+
+Note that this parameterisation of the model is related to the original
+parametersation in [1]_ by
+
+.. math::
+    V_{\text{crit}} = \frac{\lambda _1}{2\lambda _0} \quad \text{and}
+    \quad \lambda = 2\lambda _1 .
+""",  # noqa: W605
+            'model_type': 'Pharmacodynamic',
+            'sbml_url':
+            'https://raw.githubusercontent.com/pkpdapp-team/pkpdapp-datafiles/main/models/tgi_Koch_2009_reparametrised.xml'  # noqa: E501
+        },
+        {
+            'name': 'one_compartment_pk_model',
+            'description': """
+Returns the absolute path to a SBML file, specifying a one compartment pharmacokinetic model.
+
+In this model the distribution of the drug is modelled by one
+compartment with a linear elimination rate :math:`k_e`
+
+.. math ::
+    \frac{\text{d}A}{\text{d}t} = -k_e A \quad C = \frac{A}{V}.
+
+Here, :math:`A` and :math:`C` are the amount and the concentration of
+the drug in the body, respectively. :math:`V` is the effective volume
+of distribution of the drug in the compartment.
+
+This model may be interpreted as modelling the blood plasma
+concentration of the drug, with the assumption that the clearance of
+the drug through the liver may be approximated by an exponential decay
+with the rate :math:`k_e`.
+
+With a :class:`erlotinib.PharmacokineticModel` the drug may be either
+directly administered to :math:`A` or indirectly through a dosing
+compartment.
+""",  # noqa: W605
+            'model_type': 'Pharmacokinetic',
+            'sbml_url':
+            'https://raw.githubusercontent.com/pkpdapp-team/pkpdapp-datafiles/main/models/pk_one_comp.xml'  # noqa: E501
         }
     ]
 
