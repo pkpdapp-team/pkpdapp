a8f31d5d2b3639a46c8ada7827cd2ba53325d93e
Martin Robinson <martinjrobins@gmail.com>
2021-03-24 15:56:12 +0000

    #69 django plotly dash 1.3 has a bug where triggered is always empty, specify 1.4 or greater

--- setup.py
+++ setup.py
@@ -37,7 +37,7 @@ setup(
         'dash-bootstrap-components>=0.10',
         'Django==3.0.7',
         'django-bootstrap4>=2.2',
-        'django_plotly_dash>=1.3',
+        'django_plotly_dash>=1.4',
         'dpd-static-support>=0.0.5',
         'numpy>=1.8',
         'pandas>=1.0',
