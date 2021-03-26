424632d7876feea29563e1cc38bb87fc219e24cb
ben18785 <ben.c.lambert@gmail.com>
2021-03-26 10:26:50 +0000

    Changed to django_plotly_dash to 1.4.1 as later versions raise errors about `split_callback_id`

--- setup.py
+++ setup.py
@@ -37,7 +37,7 @@ setup(
         'dash-bootstrap-components>=0.10',
         'Django==3.0.7',
         'django-bootstrap4>=2.2',
-        'django_plotly_dash>=1.4',
+        'django_plotly_dash==1.4.1',
         'dpd-static-support>=0.0.5',
         'numpy>=1.8',
         'pandas>=1.0',
