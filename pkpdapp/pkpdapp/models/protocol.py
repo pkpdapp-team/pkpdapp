f97744cd69ffe8a86040f62a79815e1d01487ba2
Martin Robinson <martinjrobins@gmail.com>
2021-05-07 14:08:00 +0100

    #144 swap IV and Extravascular

--- pkpdapp/pkpdapp/models/protocol.py
+++ pkpdapp/pkpdapp/models/protocol.py
@@ -34,8 +34,8 @@ class Protocol(models.Model):
     )
 
     class DoseType(models.TextChoices):
-        DIRECT = 'D', 'Extravascular'
-        INDIRECT = 'I', 'IV'
+        DIRECT = 'D', 'IV'
+        INDIRECT = 'I', 'Extravascular'
 
     dose_type = models.CharField(
         max_length=1,
