8eb72e57f7313beee92bb20533a98b9f81019423
ben18785 <ben.c.lambert@gmail.com>
2021-05-18 11:57:44 +0100

    style and copyright

--- pkpdapp/pkpdapp/api/views.py
+++ pkpdapp/pkpdapp/api/views.py
@@ -1,4 +1,8 @@
-from django.shortcuts import render
+#
+# This file is part of PKPDApp (https://github.com/pkpdapp-team/pkpdapp) which
+# is released under the BSD 3-clause license. See accompanying LICENSE.md for
+# copyright notice and full license details.
+#
 from rest_framework import viewsets
 from .serializers import DatasetSerializer
 from pkpdapp.models import Dataset
--- pkpdapp/pkpdapp/tests/test_api.py
+++ pkpdapp/pkpdapp/tests/test_api.py
@@ -1,3 +1,8 @@
+#
+# This file is part of PKPDApp (https://github.com/pkpdapp-team/pkpdapp) which
+# is released under the BSD 3-clause license. See accompanying LICENSE.md for
+# copyright notice and full license details.
+#
 from rest_framework import status
 from rest_framework.test import APITestCase, APIClient
 
