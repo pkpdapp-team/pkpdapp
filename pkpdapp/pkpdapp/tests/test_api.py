27d5431e7349d8e3e95578f2a5a57cb7d12d11af
ben18785 <ben.c.lambert@gmail.com>
2021-05-18 11:28:11 +0100

    Update test_api.py

--- pkpdapp/pkpdapp/tests/test_api.py
+++ pkpdapp/pkpdapp/tests/test_api.py
@@ -1,12 +1,20 @@
 from rest_framework import status
-from rest_framework.authtoken.models import Token
-from rest_framework.test import APITestCase
-from django.urls import reverse
+from rest_framework.test import APITestCase, APIClient
 
 
 class DatasetTestCase(APITestCase):
+    client = APIClient()
 
     def test_dataset_creation(self):
-        data = {"name": "hello", "datatime": "", "description": ""}
+        data = {"name": "hello", "datatime": "", "description": "bye"}
         response = self.client.post("/api/datasets/", data)
         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        response_data = response.data
+        self.assertEqual(response_data["name"], data["name"])
+        self.assertEqual(response_data["description"], data["description"])
+
+        keys = response_data.keys()
+        present_keys = ['name', 'datetime', 'description', 'subjects',
+                        'biomarkertypes', 'protocols']
+        for k in present_keys:
+            self.assertTrue(k in keys)
