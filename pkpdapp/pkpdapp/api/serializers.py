5d10b8f6cc69b7dd3878dc7bf785eb62a4d6a71f
ben18785 <ben.c.lambert@gmail.com>
2021-05-18 01:35:51 +0100

    Lists biomarkertypes and subject dose-groups

--- pkpdapp/pkpdapp/api/serializers.py
+++ pkpdapp/pkpdapp/api/serializers.py
@@ -1,8 +1,21 @@
 from rest_framework import serializers
-from pkpdapp.models import Dataset
+from pkpdapp.models import (Dataset, BiomarkerType, Subject)
 
 
 class DatasetSerializer(serializers.ModelSerializer):
+    biomarkertypes = serializers.SerializerMethodField('find_biomarkertypes')
+    subject_dosegroup = serializers.SerializerMethodField(
+        'find_subject_dosegroup')
+
+    def find_biomarkertypes(self, dataset):
+        biomarkers = BiomarkerType.objects.filter(dataset=dataset)
+        return [bm.name for bm in biomarkers]
+
+    def find_subject_dosegroup(self, dataset):
+        subjects = Subject.objects.filter(dataset=dataset)
+        return [sj.dose_group for sj in subjects]
+
     class Meta:
         model = Dataset
-        fields = ('name', 'datetime', 'description')
+        fields = ('name', 'datetime', 'description',
+                  'biomarkertypes', 'subject_dosegroup')
