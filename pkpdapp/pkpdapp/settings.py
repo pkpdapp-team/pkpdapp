12e410d494e0943974dad32b8f5a988036fdf3e2
Martin Robinson <martinjrobins@gmail.com>
2021-06-15 11:22:51 +0100

    #156 dont collect static in tests

--- pkpdapp/pkpdapp/settings.py
+++ pkpdapp/pkpdapp/settings.py
@@ -201,7 +201,6 @@ STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'pkpdapp', 'static'),
 ]
 
 # Staticfiles finders for locating dash app assets and related files
