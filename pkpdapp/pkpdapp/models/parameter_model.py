a34ade20376e89e5b0c52d4019452df961cb3676
xavierlave1 <72130923+xavierlave1@users.noreply.github.com>
2021-07-26 16:29:26 +0200

    integrated template

--- pkpdapp/pkpdapp/models/parameter_model.py
+++ pkpdapp/pkpdapp/models/parameter_model.py
@@ -1,29 +1,85 @@
-#
-# This file is part of PKPDApp (https://github.com/pkpdapp-team/pkpdapp) which
-# is released under the BSD 3-clause license. See accompanying LICENSE.md for
-# copyright notice and full license details.
-#
-
-from pkpdapp.pkpdapp.models import pkpd_model
 from django.db import models
-from django.core.cache import cache
+from pkpdapp.models import (
+    Unit, DosedPharmacokineticModel,
+    PharmacokineticModel, PharmacodynamicModel,
+)
+from django.db.models import Q
 
 
-class parameter(models.Model):
+class Variable(models.Model):
+    """
+    A single variable for a mechanistic model.
+    """
 
-    SCALES = [
-        'Linear',
-        'Logarithmic',
-        'Square root'
-    ]
+    name = models.CharField(max_length=20, help_text='name of the variable')
+    pd_model = models.ForeignKey(
+        PharmacodynamicModel,
+        blank=True, null=True,
+        on_delete=models.CASCADE,
+        help_text='pharmacodynamic model'
+    )
+    pk_model = models.ForeignKey(
+        PharmacokineticModel,
+        blank=True, null=True,
+        on_delete=models.CASCADE,
+        help_text='pharmacokinetic model'
+    )
+    dosed_pk_model = models.ForeignKey(
+        DosedPharmacokineticModel,
+        blank=True, null=True,
+        on_delete=models.CASCADE,
+        help_text='dosed pharmacokinetic model'
+    )
+    unit = models.ForeignKey(
+        Unit, on_delete=models.CASCADE,
+        help_text=(
+            'variable values are in this unit'
+        )
+    )
+    lower_bound = models.FloatField(
+        default=1e-6,
+        help_text='lowest possible value for this variable'
+    )
+    upper_bound = models.FloatField(
+        default=2,
+        help_text='largest possible value for this variable'
+    )
+    default_value = models.FloatField(
+        default=1,
+        help_text='default value for this variable'
+    )
 
-    name = models.CharField(max_length=30)
-    min_value = models.FloatField()
-    max_value = models.FloatField()
-    value = models.FloatField()
-    pk_model = models.CharField(max_length=100)
-    pd_model=models.CharField(max_length=100)
-    scale = models.CharField(max_length=20, choices=SCALES)
-    unit = models.CharField(max_length=100)
+    class Scale(models.TextChoices):
+        LINEAR = 'LN', 'Linear'
+        LOG = 'LG', 'Log'
 
-    
+    scale = models.CharField(
+        max_length=2,
+        choices=Scale.choices,
+        default=Scale.LINEAR,
+    )
+
+    class Meta:
+        constraints = [
+            models.CheckConstraint(
+                check=(
+                    (Q(pk_model__isnull=True) &
+                     Q(pd_model__isnull=False) &
+                     Q(pd_model__isnull=False)) |
+                    (Q(pk_model__isnull=False) &
+                     Q(pd_model__isnull=True) &
+                     Q(pd_model__isnull=False)) |
+                    (Q(pk_model__isnull=False) &
+                     Q(pd_model__isnull=False) &
+                     Q(pd_model__isnull=True))
+                ),
+                name='variable must belong to a model'
+            ),
+            models.CheckConstraint(
+                check=(
+                    (Q(scale='LG') & Q(lower_bound__gt=0)) |
+                    Q(scale='LN')
+                ),
+                name='log scale must have a lower bound greater than zero'
+            )
+        ]
