d88ddb44f4ea227820252556d23f4b9fcbe39983
xavierlave1 <72130923+xavierlave1@users.noreply.github.com>
2021-07-30 11:55:35 +0200

    added tests for other cases

--- pkpdapp/pkpdapp/tests/test_models/test_variable.py
+++ pkpdapp/pkpdapp/tests/test_models/test_variable.py
@@ -11,11 +11,37 @@ def TestVariableModel(TestCase):
         u = Unit.objects.get(symbol='mg')
         lb = 0.001
         ub = 3.1415
+        s='LN'
+        ls='LG'
+        pk = PharmacokineticModel.objects\
+            .get(name='one_compartment_pk_model')
 
         v=Variable.objects.create(
             name=n,
             unit=u,
+            scale=s,
+            pk_model=pk,
             lower_bound=lb,
             upper_bound=ub
         )
         self.assertTrue(isinstance(v, Variable))
+
+        #missing model
+        v1=Variable.objects.create(
+            name=n,
+            unit=u,
+            scale=s,
+            lower_bound=lb,
+            upper_bound=ub
+        )
+        self.assertFalse(isinstance(v1, Variable))
+
+        #wrong log scale
+        v2=Variable.objects.create(
+            name=n,
+            unit=u,
+            scale=ls,
+            lower_bound=0,
+            upper_bound=ub
+        )
+        self.assertFalse(isinstance(v2, Variable))
