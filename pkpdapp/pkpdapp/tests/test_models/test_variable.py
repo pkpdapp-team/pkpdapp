bc7a5fec6808a6dcf1d00c82d25605d2827e5fe8
xavierlave1 <xavier.lave@gmail.com>
2021-08-23 16:02:56 +0200

    style

--- pkpdapp/pkpdapp/tests/test_models/test_variable.py
+++ pkpdapp/pkpdapp/tests/test_models/test_variable.py
@@ -12,43 +12,45 @@ from pkpdapp.models import (
 
 def TestVariableModel(TestCase):
     def test_variable_creation(self):
-        n = 'variable name'
-        u = Unit.objects.get(symbol = 'mg')
-        lb = 0.001
-        ub = 3.1415
-        s = 'LN'
-        ls = 'LG'
-        pk = PharmacokineticModel.objects\
+        n='variable name'
+        u=Unit.objects.get(symbol='mg')
+        lb=0.001
+        ub=3.1415
+        s='LN'
+        ls='LG'
+        pk=PharmacokineticModel.objects\
             .get(name='one_compartment_pk_model')
 
         v=Variable.objects.create(
-            name = n,
-            unit = u,
-            scale = s,
-            pk_model = pk,
-            lower_bound = lb,
-            upper_bound = ub
+            name=n,
+            unit=u,
+            scale=s,
+            pk_model=pk,
+            lower_bound=lb,
+            upper_bound=ub
         )
         self.assertTrue(isinstance(v, Variable))
 
         #missing model
         with self.assertRaises(IntegrityError) as context:
-            v1=Variable.objects.create(
-                name = n,
-                unit = u,
-                scale = s,
-                lower_bound = lb,
-                upper_bound = ub
+            Variable.objects.create(
+                name=n,
+                unit=u,
+                scale=s,
+                lower_bound=lb,
+                upper_bound=ub
             )
-        self.assertTrue('variable must belong to a model' in str(context.exception))
+        err_msg='variable must belong to a model'    
+        self.assertTrue(err_msg in str(context.exception))
 
         #wrong log scale
         with self.assertRaises(IntegrityError) as context:
-            v2=Variable.objects.create(
-                name = n,
-                unit = u,
-                scale = ls,
-                lower_bound = 0,
-                upper_bound = ub
+            Variable.objects.create(
+                name=n,
+                unit=u,
+                scale=ls,
+                lower_bound=0,
+                upper_bound=ub
             )
-        self.assertTrue('log scale must have a lower bound greater than zero' in str(context.exception))
+        err_msg='log scale must have a lower bound greater than zero'
+        self.assertTrue(err_msg in str(context.exception))
