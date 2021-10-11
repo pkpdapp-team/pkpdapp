f51985c364322ecb8cab482caa5dea455cd4bcac
ben18785 <ben.c.lambert@gmail.com>
2021-10-11 13:54:43 +0100

    added in a sketch of the inference mixin class

--- pkpdapp/pkpdapp/models/inference.py
+++ pkpdapp/pkpdapp/models/inference.py
@@ -266,3 +266,91 @@ class MCMCDiagnostic(models.Model):
 class Rhat(MCMCDiagnostic):
 
 class ESS(MCMCDiagnostic):
+
+
+class InferenceMixin:
+    # the instantiation will look different for this
+    # since all the current inputs will be accessed from
+    # the PKPD model DB in Django. These are just effectively
+    # a list of all the items that will need to be accessible
+    # by this class
+    def __init__(self):
+
+        self._log_likelihood_method_dict = {
+            'ConstantAndMultiplicative': pints.ConstantAndMultiplicativeLogLikelihood,
+            'GaussianKnownSigma': pints.GaussianKnownSigmaLogLikelihood,
+            'GaussianUnknownSigma': pints.GaussianUnknownSigmaLogLikelihood,
+            'LogNormal': pints.LogNormalLogLikelihood,
+            'StudentT': pints.StudentTLogLikelihood
+        }
+
+        self._log_prior_method_dict = {
+            'Exponential': pints.ExponentialLogPrior,
+            'Uniform': pints.UniformLogPrior,
+            'Gaussian': pints.GaussianLogPrior
+        }
+
+        self._optimisers_dict = {
+            'CMAES': pints.CMAES,
+            'Nelder-Mead': pints.NelderMead,
+            'PSO': pints.PSO,
+            'SNES': pints.SNES,
+            'XNES': pints.XNES
+        }
+
+        self._samplers_dict = {
+            'Adaptive covariance': pints.HaarioBardenetACMC,
+            'Differential evolution': pints.DifferentialEvolutionMCMC,
+            'DRAM': pints.DRAM,
+            'DREAM': pints.DREAM,
+            'Emcee-hammer': pints.EmceeHammerMCMC,
+            'Population': pints.PopulationMCMC
+        }
+
+    def create_pints_forward_model(self):
+        model = MyokitForwardModel(self._myokit_model,
+                                   self._myokit_simulator,
+                                   self._outputs_observed,
+                                   self._fixed_parameters)
+        self._pints_forward_model = model
+
+    def get_myokit_model(self):
+        return self._myokit_model
+
+    def get_parameter_bounds(self):
+        return self._bounds
+
+    def create_pints_problem(self):
+        model = self._model
+        times = self._times
+        values = self._values
+        if model.n_outputs() == 1:
+            self._pints_problem = pints.SingleOutputProblem(model, times, values)
+        else:
+            self._pints_problem = pints.MultiOutputProblem(model, times, values)
+
+    def create_pints_log_likelihood(self):
+        likelihood_method = self._log_likelihood_dict(self._log_likelihood_name)
+        self._pints_log_likelihood = likelihood_method(self._pints_problem)
+
+        # at this point, we will need to do something to create the extra vars
+        # to store in Xavier's DB
+
+    def create_pints_log_prior(self):
+        self._log_prior = pints.ComposedLogPrior(self._log_priors)
+
+    def create_pints_log_posterior(self):
+        self._pints_log_posterior = pints.LogPosterior(self._pints_log_likelihood,
+                                                       self._pints_log_prior)
+
+    def create_pints_optimiser(self):
+        self._optimiser = h
+
+    def create_pints_sampler(self):
+        pass
+
+    def run_inference(self, controller):
+        pass
+
+    def fixed_parameters(self):
+        return self._fixed_parameters
