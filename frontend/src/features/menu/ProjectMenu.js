4714cfaa590a7b03141a00a25de0f8e136b20132
Martin Robinson <martinjrobins@gmail.com>
2021-10-13 16:28:26 +0100

    #190 remove unneeded imports

--- frontend/src/features/menu/ProjectMenu.js
+++ frontend/src/features/menu/ProjectMenu.js
@@ -1,8 +1,4 @@
 import React from "react";
-import { makeStyles } from '@material-ui/core/styles';
-import {
-  Link, matchPath, useLocation
-} from "react-router-dom";
 
 import { useSelector } from 'react-redux'
 import AccountTreeIcon from '@material-ui/icons/AccountTree';
@@ -14,18 +10,6 @@ import ListItemText from '@material-ui/core/ListItemText';
 
 import Divider from '@material-ui/core/Divider';
 
-import Collapse from '@material-ui/core/Collapse';
-import ExpandLess from '@material-ui/icons/ExpandLess';
-import ExpandMore from '@material-ui/icons/ExpandMore';
-
-import ListIcon from '@material-ui/icons/List';
-
-import VisibilityIcon from '@material-ui/icons/Visibility';
-import TimelineIcon from '@material-ui/icons/Timeline';
-import CheckBoxOutlineBlankIcon from '@material-ui/icons/CheckBoxOutlineBlank';
-import BubbleChartIcon from '@material-ui/icons/BubbleChart';
-import SettingsBackupRestoreIcon from '@material-ui/icons/SettingsBackupRestore';
-
 import Datasets from '../datasets/Datasets' 
 import PkModels from '../pkModels/PkModels' 
 import PdModels from '../pdModels/PdModels' 
@@ -36,20 +20,7 @@ import {
 } from '../projects/projectsSlice.js'
 
 
-
-const useStyles = makeStyles((theme) => ({
-  nested: {
-    paddingLeft: theme.spacing(4),
-  },
-}));
-
-
 export default function ProjectMenu() {
-  const classes = useStyles();
-  const [dataAnalysisOpen, setDataAnalysisOpen] = React.useState(false);
-  const handleDataAnalysisClick = () => {
-    setDataAnalysisOpen((open) => !open);
-  };
   const project = useSelector(selectChosenProject);
   console.log('got project', project)
 
