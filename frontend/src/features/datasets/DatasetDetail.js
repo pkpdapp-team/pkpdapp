c1ceef9b76e734426cdcc61a2a8b9c83257cd83c
Martin Robinson <martinjrobins@gmail.com>
2021-10-19 11:32:50 +0100

    #190 clean up

--- frontend/src/features/datasets/DatasetDetail.js
+++ frontend/src/features/datasets/DatasetDetail.js
@@ -13,7 +13,6 @@ import Accordion from '@material-ui/core/Accordion';
 import AccordionSummary from '@material-ui/core/AccordionSummary';
 import AccordionDetails from '@material-ui/core/AccordionDetails';
 import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
-import LinearProgress from '@material-ui/core/LinearProgress'
 
 import SubjectSubform from './SubjectSubform'
 import BiomarkerTypeSubform from './BiomarkerTypeSubform'
