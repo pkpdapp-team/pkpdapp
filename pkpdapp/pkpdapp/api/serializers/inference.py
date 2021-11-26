625f80e82613d512054e8f3afef1a8a8d1c6879b
martinjrobins <martinjrobins@gmail.com>
2021-11-26 17:18:11 +0000

    #222 abandon nested serializer

--- pkpdapp/pkpdapp/api/serializers/inference.py
+++ pkpdapp/pkpdapp/api/serializers/inference.py
@@ -22,9 +22,7 @@ class AlgorithmSerializer(serializers.ModelSerializer):
         fields = '__all__'
 
 
-class InferenceSerializer(
-    WritableNestedModelSerializer
-):
+class InferenceSerializer(serializers.ModelSerializer):
     pd_model_detail = PharmacodynamicSerializer(
         source='pd_model', read_only=True
     )
@@ -34,36 +32,10 @@ class InferenceSerializer(
     pkpd_model_detail = PkpdSerializer(
         source='pkpd_model', read_only=True
     )
-    priors = PriorSerializer(many=True)
-    objective_functions = ObjectiveFunctionSerializer(many=True)
 
     class Meta:
         model = Inference
         fields = '__all__'
-
-    #def update(self, instance, validated_data):
-    #    for name, Serializer in [
-    #            ('priors', PriorSerializer),
-    #            ('objective_functions', ObjectiveFunctionSerializer)
-    #    ]:
-    #        field_datas = validated_data.pop(name)
-    #        old_models = (instance.priors).all()
-    #        old_models_map = {p.id: p for p in old_models}
-
-    #        for field_data in field_datas:
-    #            serializer = Serializer(field_data)
-    #            serializer.is_valid()
-    #            field_validated_data = serializer.validated_data
-    #            if 'id' in field_data:
-    #                old_field = old_models_map[field_data['id']]
-    #                field = serializer.update(
-    #                    old_field, field_validated_data
-    #                )
-    #            else:
-    #                field = serializer.create(field_validated_data)
-    #            field.save()
-
-    #    return InferenceSerializer.update(instance, validated_data)
 
 
 class InferenceChainSerializer(serializers.ModelSerializer):
