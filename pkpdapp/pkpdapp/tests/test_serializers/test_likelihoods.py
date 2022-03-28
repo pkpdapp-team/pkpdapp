4ea51cfbfb76a03ff35b245cea431a887f04012c
martinjrobins <martinjrobins@gmail.com>
2022-03-28 11:19:32 +0100

    #245 coverage

--- pkpdapp/pkpdapp/tests/test_serializers/test_likelihoods.py
+++ pkpdapp/pkpdapp/tests/test_serializers/test_likelihoods.py
@@ -15,7 +15,7 @@ from pkpdapp.api.serializers import (
 )
 
 
-class TestObjectiveFunctionSerializer(TestCase):
+class TestLoglikelihoodSerializer(TestCase):
     def setUp(self):
         project = Project.objects.get(
             name='demo',
@@ -37,7 +37,8 @@ class TestObjectiveFunctionSerializer(TestCase):
             inference=self.inference,
             biomarker_type=self.biomarker_type
         )
-        self.parameter = self.log_likelihood.parameters.first()
+        self.parameters = self.log_likelihood.parameters.all()
+        parameter = self.parameters[0]
         LogLikelihood.objects.create(
             form='LN',
             variable=self.variables[0],
@@ -47,7 +48,7 @@ class TestObjectiveFunctionSerializer(TestCase):
         PriorUniform.objects.create(
             lower=0.0,
             upper=1.0,
-            log_likelihood_parameter=self.parameter
+            log_likelihood_parameter=parameter
         )
 
     def test_serialize(self):
@@ -72,14 +73,25 @@ class TestObjectiveFunctionSerializer(TestCase):
     def test_update(self):
         serializer = LogLikelihoodSerializer(self.log_likelihood)
         data = serializer.data
-        found_index = -1
+        found_index_first = -1
+        found_index_second = -1
         for i in range(len(data['parameters'])):
-            if data['parameters'][i]['name'] == self.parameter.name:
-                found_index = i
-                break
+            if data['parameters'][i]['name'] == self.parameters[0].name:
+                found_index_first = i
+            if data['parameters'][i]['name'] == self.parameters[1].name:
+                found_index_second = i
 
-        data['parameters'][found_index]['value'] = 2.0
+        data['parameters'][found_index_first]['value'] = 2.0
+        data['parameters'][found_index_second]['prior'] = {
+            'type': 'PriorUniform',
+            'lower': 0.1,
+            'upper': 1.1,
+        }
         validated_data = serializer.to_internal_value(data)
         serializer.update(self.log_likelihood, validated_data)
-        self.parameter.refresh_from_db()
-        self.assertEqual(self.parameter.value, 2.0)
+        self.parameters[0].refresh_from_db()
+        self.parameters[1].refresh_from_db()
+        self.assertEqual(self.parameters[0].value, 2.0)
+        self.assertIsInstance(self.parameters[1].prior, PriorUniform)
+        self.assertEqual(self.parameters[1].prior.lower, 0.1)
+        self.assertEqual(self.parameters[1].prior.upper, 1.1)
