424e92e8d58eb0f85f477c75a1b24a6795e520c6
martinjrobins <martinjrobins@gmail.com>
2022-09-23 17:17:48 +0100

    #272 first stab at parser

---
+++ pkpdapp/pkpdapp/tests/test_utils/test_monolix_parser.py
@@ -0,0 +1,33 @@
+#
+# This file is part of PKPDApp (https://github.com/pkpdapp-team/pkpdapp) which
+# is released under the BSD 3-clause license. See accompanying LICENSE.md for
+# copyright notice and full license details.
+#
+
+import codecs
+import unittest
+import urllib.request
+from urllib.request import urlretrieve
+from pkpdapp.utils import MonolixParser
+
+from django.test import TestCase
+from django.utils import timezone
+
+from pkpdapp.models import Biomarker, BiomarkerType, Dataset, Unit
+
+class TestMonolixParser(unittest.TestCase):
+    def test_parse(self):
+        BASE_URL_DATASETS = 'https://raw.githubusercontent.com/pkpdapp-team/pkpdapp-datafiles/main/'   # noqa: E501
+        with urllib.request.urlopen(
+            BASE_URL_DATASETS + 'usecase_monolix/PK_Model.txt', timeout=5
+        ) as f:
+            monolix_str = codecs.decode(f.read(), 'utf-8')
+            parser = MonolixParser()
+            parser.parse(monolix_str)
+            print('model is', parser.myokit_model)
+            print(parser.myokit_model.code())
+            parser.myokit_model.validate()
+
+
+
+
---
+++ pkpdapp/pkpdapp/utils/monolix_parser.py
@@ -0,0 +1,291 @@
+#
+# This file is part of PKPDApp (https://github.com/pkpdapp-team/pkpdapp) which
+# is released under the BSD 3-clause license. See accompanying LICENSE.md for
+# copyright notice and full license details.
+#
+
+# based on fourFn.py example from pyparsing
+# (https://github.com/pyparsing/pyparsing/blob/master/examples/fourFn.py)
+# Copyright 2003-2019 by Paul McGuire
+#
+from dataclasses import dataclass
+
+import myokit
+import pyparsing as pp
+import pyparsing.common as ppc
+
+from pkpdapp.models import set_administration
+pp.enable_all_warnings()
+
+binops = {
+    '/': myokit.Divide,
+    '*': myokit.Multiply,
+    '-': myokit.Minus,
+    '+': myokit.Plus,
+}
+
+@dataclass
+class Variable:
+    name: str
+    constant: bool
+    state: bool
+
+class MonolixParser:
+    """
+    expop   :: '^'
+    multop  :: '*' | '/'
+    addop   :: '+' | '-'
+    integer :: ['+' | '-'] '0'..'9'+
+    atom    :: real
+               | ident
+               | '(' expr ')'
+    factor  :: atom [ expop factor ]*
+    term    :: factor [ multop factor ]*
+    expr    :: term [ addop term ]*
+    eqn     :: term
+    """
+    # map operator symbols to corresponding arithmetic operations
+
+
+    def __init__(self):
+        # fnumber = Combine(Word("+-"+nums, nums) +
+        #                    Optional("." + Optional(Word(nums))) +
+        #                    Optional(e + Word("+-"+nums, nums)))
+        # or use provided pyparsing_common.number, but convert back to str:
+        fnumber = ppc.number()
+        ident = pp.Regex("[_a-zA-Z][_a-zA-Z0-9]*")
+        ident_0 = pp.Regex("[_a-zA-Z][_a-zA-Z0-9]*_0")
+        ddt_ident = pp.Regex("ddt_[_a-zA-Z][_a-zA-Z0-9]*")
+
+        plus, minus, mult, div = map(pp.Literal, "+-*/")
+        lpar, rpar = map(pp.Suppress, "()")
+        lcpar, rcpar = map(pp.Suppress, "{}")
+        addop = plus | minus
+        multop = mult | div
+        expop = pp.Literal("^")
+        equals = pp.Suppress("=")
+
+        expr = pp.Forward()
+        atom = (
+            addop[...] + (
+                (fnumber | ident).set_parse_action(self.push_first)
+                | pp.Group(lpar + expr + rpar)
+            )
+        ).set_parse_action(self.push_unary_minus)
+
+        # by defining exponentiation as "atom [ ^ factor ]..." instead of "atom
+        # [ ^ atom ]...", we get right-to-left exponents, instead of
+        # left-to-right that is, 2^3^2 = 2^(3^2), not (2^3)^2.
+        factor = pp.Forward()
+        factor <<= atom + (expop + factor).set_parse_action(self.push_first)[...]
+        term = factor + (multop + factor).set_parse_action(self.push_first)[...]
+        expr <<= term + (addop + term).set_parse_action(self.push_first)[...]
+
+        ode_type = pp.Keyword('odeType') + equals + (pp.Keyword('stiff') |
+                                                     pp.Keyword('nonStiff'))
+        equation = (
+            ident + equals +
+            pp.Group(expr).set_parse_action(self.construct_expr)
+        )
+        initial_condition = (
+            ident_0 + equals +
+            pp.Group(expr).set_parse_action(self.construct_expr)
+        )
+        rate_equation = (
+            ddt_ident + equals +
+            pp.Group(expr).set_parse_action(self.construct_expr)
+        )
+
+        fn_value = fnumber | ident
+        fn_arg = (ident + equals + fn_value) | fn_value
+        fn_call = ident + lpar - pp.delimited_list(pp.Group(fn_arg), delim=',') + rpar
+
+        pk_block = ("PK:" + (
+            pp.Group(fn_call)
+        )[...]).set_parse_action(self.construct_pk)
+
+        equation_block = pp.Keyword("EQUATION:") + (
+            ode_type
+            | equation.set_parse_action(self.construct_equation)
+            | rate_equation.set_parse_action(self.construct_rate_equation)
+            | initial_condition.set_parse_action(self.construct_initial_condition)
+        )[...]
+
+        output_block = pp.Keyword("OUTPUT:") + (
+            ident + equals + lcpar - pp.delimited_list(ident, delim=',') + rcpar
+        )
+
+        inputs = (
+            pp.Keyword("input") + equals + lcpar
+            - pp.delimited_list(
+                ident, delim=','
+            ).set_parse_action(self.construct_inputs)
+            + rcpar
+        )
+
+        longitudinal_keyword = pp.Keyword("[LONGITUDINAL]")
+
+        longitudinal =  (
+            longitudinal_keyword
+            + inputs.set_debug(True)
+            + (pk_block & equation_block & output_block).set_debug(True)
+        )
+
+        src =  "DESCRIPTION:" + pp.SkipTo(longitudinal_keyword) + longitudinal
+
+        monolix_comment = ';' + pp.restOfLine()
+        src.ignore(monolix_comment)
+
+        pp.autoname_elements()
+        self.parser = src
+        self.exprStack = []
+        self.variables = {}
+        self.pk = []
+        self.myokit_model = myokit.Model()
+        root_cmp = self.myokit_model.add_component('root')
+        t = self.get_or_construct_var('t', root_cmp)
+        t.set_binding('time')
+
+        self.administration_id = None
+        self.tlag = None
+
+
+    def push_first(self, toks):
+        self.exprStack.append(toks[0])
+
+    def push_unary_minus(self, toks):
+        for t in toks:
+            if t == "-":
+                self.exprStack.append("unary -")
+            else:
+                break
+
+    def construct_pk(self, toks):
+        print('construct_pk', toks)
+        compartments = {}
+        for line in toks[1:]:
+            print('line', line)
+            fn_name, *tok_args = line
+            args = {}
+            for i, arg in enumerate(tok_args):
+                if len(arg) == 2:
+                    [name, value] = arg
+                    args[name] = value
+                else:
+                    [value] = arg
+                    args[i] = value
+            print('args', args)
+
+            if fn_name == 'compartment':
+                cmt = args.get('cmt', args.get(0, 1))
+                if not isinstance(cmt, str) or cmt[0].isalpha():
+                    cmt = f'cmt{cmt}'
+                amount = args.get('amount', args.get(1, None))
+                volume = args.get('volume', args.get(2, 1))
+                concentration = args.get('concentration', args.get(3, None))
+                comp = self.myokit_model.add_component(cmt)
+                if isinstance(volume, str):
+                    volume_var = self.get_or_construct_var(volume, comp)
+                else:
+                    volume_var = myokit.Number(volume)
+                if amount:
+                    amount_var = self.get_or_construct_var(amount, comp)
+                else:
+                    raise pp.ParseException(f'arg "amount" required for compartment {cmt}')
+                if concentration:
+                    conc_var = self.get_or_construct_var(concentration, comp)
+                    conc_var.set_rhs(myokit.Divide(amount_var, volume_var))
+                compartments[cmt] = amount_var
+
+            elif fn_name == 'iv':
+                cmt = args.get('cmt', args.get(0, 1))
+                if not isinstance(cmt, str) or cmt[0].isalpha():
+                    cmt = f'cmt{cmt}'
+                adm = args.get('adm', args.get(1, 1))
+                if self.administration_id is not None:
+                    raise pp.ParseException(
+                        'only a single administration id supported'
+                    )
+                self.administration_id = adm
+                tlag = args.get('Tlag', args.get(2, 0))
+                self.tlag = tlag
+                # p = args.get('p', args.get(3, 1))
+
+                if cmt not in compartments:
+                    raise pp.ParseException(f'compartment {cmt} not found in pk block')
+                amount_var = compartments[cmt]
+                if not amount_var.is_state():
+                    amount_var.promote(0)
+                direct = True
+                set_administration(
+                    self.myokit_model, cmt, direct=direct,
+                    amount_var=amount_var.name()
+                )
+
+    def construct_inputs(self, toks):
+        print('construct_inputs', toks)
+        for name in toks:
+            v = self.get_or_construct_var(name)
+
+    def construct_expr(self, toks):
+        toks[0] = self.construct_myokit_expr(self.exprStack)
+
+    def get_or_construct_var(self, name, compartment=None):
+        if compartment is None:
+            compartment = self.myokit_model.get('root')
+        if name not in self.variables:
+            var = compartment.add_variable(name)
+            var.set_rhs(0)
+            var.set_unit(myokit.units.dimensionless)
+            self.variables[name] = var
+        return self.variables[name]
+
+    def construct_equation(self, toks):
+        print('construct_equation', toks)
+        name, expr = toks
+        var = self.get_or_construct_var(name)
+        print('expr', expr)
+        var.set_rhs(expr)
+
+    def construct_rate_equation(self, toks):
+        print('construct_rate_equation', toks)
+        name, expr = toks
+        var = self.get_or_construct_var(name)
+        if not var.is_state():
+            var.promote(0)
+        var.set_rhs(expr)
+
+    def construct_initial_condition(self, toks):
+        print('construct_initial_condition', toks)
+        name, expr = toks
+        var = self.get_or_construct_var(name)
+        if not var.is_state():
+            var.promote(0)
+        var.set_rhs(expr)
+
+    def construct_myokit_expr(self, s):
+        op = s.pop()
+        print('op', op)
+        if op == "unary -":
+            return -self.construct_myokit_expr(s)
+        if op in "+-*/^":
+            # note: operands are pushed onto the stack in reverse order
+            op2 = self.construct_myokit_expr(s)
+            op1 = self.construct_myokit_expr(s)
+            return binops[op](op1, op2)
+        elif op[0].isalpha():
+            var = self.get_or_construct_var(op)
+            return myokit.Name(var)
+        else:
+            # try to evaluate as int first, then as float if int fails
+            try:
+                return int(op)
+            except ValueError:
+                return float(op)
+
+    def parse(self, string, parseAll=True):
+        try:
+            self.parser.parseString(string, parseAll=parseAll)
+        except pp.ParseException as err:
+            print(err.explain())
+
