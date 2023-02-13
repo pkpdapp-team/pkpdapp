fd8fd3184313d2945fca43f91893b7284873b8e5
martinjrobins <martinjrobins@gmail.com>
2023-02-13 12:04:16 +0000

    #272 fixees to data parser

--- pkpdapp/pkpdapp/utils/data_parser.py
+++ pkpdapp/pkpdapp/utils/data_parser.py
@@ -2,6 +2,8 @@ import pandas as pd
 from pkpdapp.models import Unit
 from io import StringIO
 
+from pkpdapp.pkpdapp.models.subject import Subject
+
 
 
  
@@ -16,6 +18,9 @@ class DataParser:
         "OBSERVATION_NAME": ["Observation_id", "YTYPE", "YNAME"],
         "OBSERVATION_UNIT": ["Observation_unit", "YDESC", "YUNIT"],
         "DOSE_GROUP": ["Dose_cat", "Dose_group", "DOSEGROUP"],
+        "COMPOUND": ["Compound", "COMPOUND"],
+        "ROUTE": ["Route", "ROUTE"],
+        "INFUSION_TIME": ["TINF", "Tinf", "tinf", "Infusion_time", "INFUSIONTIME", "INFUSION_TIME"],
     }
 
     required_cols = [
@@ -31,6 +36,9 @@ class DataParser:
         "AMOUNT_UNIT",
         "OBSERVATION_UNIT",
         "OBSERVATION_NAME",
+        "COMPOUND",
+        "ROUTE",
+        "INFUSION_TIME",
     ]
 
     altername_unit_names = {
@@ -90,8 +98,31 @@ class DataParser:
         for unit_col in ["TIME_UNIT", "AMOUNT_UNIT", "OBSERVATION_UNIT"]:
             if unit_col in found_cols:
                 data[unit_col] = data[unit_col].map(map_unit_names)
+                
+        # put in default observation name if not present
+        if "OBSERVATION_NAME" not in found_cols:
+            data["OBSERVATION_NAME"] = "observation"
+            
+        # put in default compound name if not present
+        if "COMPOUND" not in found_cols:
+            data["COMPOUND"] = "unknown compound"
+            
+        # put in default route name if not present
+        if "ROUTE" not in found_cols:
+            data["ROUTE"] = "IV"
+            
+        # put in default units if not present
+        for unit_col in ["TIME_UNIT", "AMOUNT_UNIT", "OBSERVATION_UNIT"]:
+            if unit_col not in found_cols:
+                data[unit_col] = ""
+                
+        # put in default infusion time if not present
+        min_delta_time = data["TIME", "SUBJECT_ID"].unique().sort_values(by=["SUBJECT_ID", "TIME"]).diff().min()
+        if "INFUSION_TIME" not in found_cols:
+            data["INFUSION_TIME"] = min_delta_time / 100.0
 
         # check that units are in database
+        found_units = {}
         for unit_col in ["TIME_UNIT", "AMOUNT_UNIT", "OBSERVATION_UNIT"]:
             if unit_col in found_cols:
                 units = data[unit_col].unique().tolist()
@@ -109,6 +140,41 @@ class DataParser:
                             'contains the following unknown units: {}'
                         ).format(error_units)
                     )
+                
+        # check that time unit is constant
+        if "TIME_UNIT" in found_cols:
+            time_units = data["TIME_UNIT"].unique()
+            if len(time_units) > 1:
+                raise RuntimeError(
+                    (
+                        'Error parsing file, '
+                        'contains multiple time units: {}'
+                    ).format(time_units)
+                )
+        
+        # check that units are same for each observation type
+        if "OBSERVATION_UNIT" in found_cols:
+            obs_units = data.groupby("OBSERVATION_ID")["OBSERVATION_UNIT"].unique()
+            obs_units = obs_units.apply(lambda x: x[0])
+            if len(obs_units.unique()) > 1:
+                raise RuntimeError(
+                    (
+                        'Error parsing file, '
+                        'contains multiple observation units: {}'
+                    ).format(obs_units)
+                )
+            
+        # check that dose group is constant for each subject id
+        if "DOSE_GROUP" in found_cols:
+            dose_groups = data.groupby("ID")["DOSE_GROUP"].unique()
+            dose_groups = dose_groups.apply(lambda x: x[0])
+            if len(dose_groups.unique()) > 1:
+                raise RuntimeError(
+                    (
+                        'Error parsing file, '
+                        'contains multiple dose groups: {}'
+                    ).format(dose_groups)
+                )
 
         # check for missing data and drop any rows where data are missing
         num_missing = data.isna().sum().sum()
@@ -122,4 +188,7 @@ class DataParser:
         data = pd.read_csv(data, delimiter=delimiter)
         data = self.validate(data)
         return data
+    
+    
+
     
