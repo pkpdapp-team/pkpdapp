17ccdae990bf561d3c59871bf016698f8800b25f
martinjrobins <martinjrobins@gmail.com>
2023-02-14 13:31:20 +0000

    #272 copyright

--- pkpdapp/pkpdapp/utils/data_parser.py
+++ pkpdapp/pkpdapp/utils/data_parser.py
@@ -1,3 +1,9 @@
+#
+# This file is part of PKPDApp (https://github.com/pkpdapp-team/pkpdapp) which
+# is released under the BSD 3-clause license. See accompanying LICENSE.md for
+# copyright notice and full license details.
+#
+
 import pandas as pd
 from pkpdapp.models import Unit
 from io import StringIO
