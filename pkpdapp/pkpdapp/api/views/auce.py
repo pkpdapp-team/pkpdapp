0645b8d8c65b0319b277744fd6cb6610a7d3f1e4
martinjrobins <martinjrobins@gmail.com>
2023-11-29 14:59:09 +0000

    flake8

--- pkpdapp/pkpdapp/api/views/auce.py
+++ pkpdapp/pkpdapp/api/views/auce.py
@@ -3,9 +3,7 @@
 # is released under the BSD 3-clause license. See accompanying LICENSE.md for
 # copyright notice and full license details.
 #
-from rest_framework import (
-    views, status
-)
+from rest_framework import views, status
 from rest_framework.response import Response
 from pkpdapp.models import (
     BiomarkerType,
@@ -15,104 +13,96 @@ from pkpdapp.utils import Auce
 
 required_text = "This field is required"
 
+
 class AuceView(views.APIView):
     def post(self, request, format=None):
-        errors = {
-        }
+        errors = {}
 
-        biomarker_type_id = request.data.get('biomarker_type_id', None)
+        biomarker_type_id = request.data.get("biomarker_type_id", None)
         if biomarker_type_id is None:
-            errors['biomarker_type_id'] = required_text
+            errors["biomarker_type_id"] = required_text
         else:
             try:
-                biomarker_type = \
-                    BiomarkerType.objects.get(id=biomarker_type_id)
+                biomarker_type = BiomarkerType.objects.get(id=biomarker_type_id)
             except BiomarkerType.DoesNotExist:
-                errors['biomarker_type_id'] = (
-                    "BiomarkerType id {} not found"
-                    .format(biomarker_type_id)
+                errors["biomarker_type_id"] = "BiomarkerType id {} not found".format(
+                    biomarker_type_id
                 )
 
-        group_type_id = request.data.get('group_type_id', None)
+        group_type_id = request.data.get("group_type_id", None)
         if group_type_id is None:
-            errors['group_type_id'] = required_text
+            errors["group_type_id"] = required_text
         else:
             try:
-                group_type = \
-                    BiomarkerType.objects.get(id=group_type_id)
+                group_type = BiomarkerType.objects.get(id=group_type_id)
             except BiomarkerType.DoesNotExist:
-                errors['group_type_id'] = (
-                    "BiomarkerType id {} not found"
-                    .format(biomarker_type_id)
+                errors["group_type_id"] = "BiomarkerType id {} not found".format(
+                    biomarker_type_id
                 )
 
-        concentration_type_id = request.data.get('concentration_type_id', None)
+        concentration_type_id = request.data.get("concentration_type_id", None)
         if concentration_type_id is None:
-            errors['concentration_type_id'] = required_text
+            errors["concentration_type_id"] = required_text
         else:
             try:
-                concentration_type = \
-                    BiomarkerType.objects.get(id=concentration_type_id)
+                concentration_type = BiomarkerType.objects.get(id=concentration_type_id)
             except BiomarkerType.DoesNotExist:
-                errors['group_type_id'] = (
-                    "BiomarkerType id {} not found"
-                    .format(concentration_type_id)
+                errors["group_type_id"] = "BiomarkerType id {} not found".format(
+                    concentration_type_id
                 )
 
         if errors:
-            return Response(
-                errors, status=status.HTTP_400_BAD_REQUEST
-            )
+            return Response(errors, status=status.HTTP_400_BAD_REQUEST)
 
         group_data = group_type.data(first_time_only=True)
-        groups = list(group_data['values'].unique()) + [None]
+        groups = list(group_data["values"].unique()) + [None]
         obs_data = biomarker_type.data()
         concentration_data = concentration_type.data(first_time_only=True)
 
         auces = []
         for group in groups:
             if group is None:
-                group_subjects = group_data['subjects'].unique()
+                group_subjects = group_data["subjects"].unique()
             else:
-                group_subjects = group_data.loc[
-                    group_data['values'] == group
-                ]['subjects']
+                group_subjects = group_data.loc[group_data["values"] == group][
+                    "subjects"
+                ]
             group_concentrations = concentration_data[
-                concentration_data['subjects'].isin(group_subjects)
+                concentration_data["subjects"].isin(group_subjects)
             ]
-            group_obs_data = obs_data[
-                obs_data['subjects'].isin(group_subjects)
-            ]
+            group_obs_data = obs_data[obs_data["subjects"].isin(group_subjects)]
 
             subject_times = []
             subject_data = []
             concentrations = []
             subject_ids = []
             for subject in group_subjects:
-                times = group_obs_data.loc[
-                    group_obs_data['subjects'] == subject
-                ]['times']
-                values = group_obs_data.loc[
-                    group_obs_data['subjects'] == subject
-                ]['values']
+                times = group_obs_data.loc[group_obs_data["subjects"] == subject][
+                    "times"
+                ]
+                values = group_obs_data.loc[group_obs_data["subjects"] == subject][
+                    "values"
+                ]
                 concentration = group_concentrations.loc[
-                    group_concentrations['subjects'] == subject
-                ]['values'].iloc[0]
+                    group_concentrations["subjects"] == subject
+                ]["values"].iloc[0]
                 concentrations.append(concentration)
                 subject_times.append(times)
                 subject_data.append(values)
                 subject_ids.append(subject)
-            auces.append(Auce(
-                group if group is not None else 'All',
-                subject_ids, concentrations,
-                subject_times, subject_data
-            ))
+            auces.append(
+                Auce(
+                    group if group is not None else "All",
+                    subject_ids,
+                    concentrations,
+                    subject_times,
+                    subject_data,
+                )
+            )
 
         serializers = [AuceSerializer(auce) for auce in auces]
 
         if errors:
-            return Response(
-                errors, status=status.HTTP_400_BAD_REQUEST
-            )
+            return Response(errors, status=status.HTTP_400_BAD_REQUEST)
 
         return Response([serializer.data for serializer in serializers])
