63e0603336f291eb30e10ba69bb780988a87b603
martinjrobins <martinjrobins@gmail.com>
2024-01-22 16:44:19 +0000

    #337 add update models and units scripts

---
+++ pkpdapp/update_units.py
@@ -0,0 +1,15 @@
+import os
+
+PROJECT_NAME = 'pkpdapp'
+
+def main():
+    from django.apps import apps
+    load_units= __import__('pkpdapp.migrations.0007_initial_units', fromlist=['load_units']).load_units
+    load_units(apps, None)
+    
+
+if __name__ == '__main__':
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', '%s.settings' % PROJECT_NAME)
+    import django
+    django.setup()
+    main()
