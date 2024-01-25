da6a391552e6f85eb13ecb6fd9cbd78bb1c36c64
martinjrobins <martinjrobins@gmail.com>
2024-01-25 15:47:05 +0000

    #337 flake8

--- pkpdapp/update_models.py
+++ pkpdapp/update_models.py
@@ -5,17 +5,21 @@
 #
 import os
 
-PROJECT_NAME = 'pkpdapp'
+PROJECT_NAME = "pkpdapp"
+
 
 def main():
     from django.apps import apps
-    load_pkpd_models = __import__('pkpdapp.migrations.0006_initial_pkpd_models', fromlist=['load_pkpd_models']).load_pkpd_models
-    load_pkpd_models(apps, None)
-    
 
-if __name__ == '__main__':
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', '%s.settings' % PROJECT_NAME)
+    load_pkpd_models = __import__(
+        "pkpdapp.migrations.0006_initial_pkpd_models", fromlist=["load_pkpd_models"]
+    ).load_pkpd_models
+    load_pkpd_models(apps, None)
+
+
+if __name__ == "__main__":
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "%s.settings" % PROJECT_NAME)
     import django
+
     django.setup()
     main()
- 
