f8d7a71b5f6ee695bae9605687fdfb382da6c39d
martinjrobins <martinjrobins@gmail.com>
2024-02-06 17:06:27 +0000

    fix start-server

--- start-server.sh
+++ start-server.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 # start-server.sh
 (python manage.py migrate --no-input)
-(python manage.py update_models.py)
+(python update_models.py)
 (celery -A pkpdapp worker --loglevel=INFO) &
 (gunicorn pkpdapp.wsgi:application --bind unix:/run/gunicorn.socket --workers 3) &
 nginx -g "daemon off;"
