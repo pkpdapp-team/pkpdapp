d030a9b77eb86fe142a3307eee42a4065c6dc4f4
Jim O'Donnell <james.odonnell@dtc.ox.ac.uk>
2024-03-15 15:44:41 +0000

    Test simulations with a project and dataset

--- pkpdapp/pkpdapp/tests/test_views/test_simulate.py
+++ pkpdapp/pkpdapp/tests/test_views/test_simulate.py
@@ -5,9 +5,17 @@
 #
 
 from pkpdapp.models import (
-    PharmacodynamicModel, Variable,
+    PharmacodynamicModel,
+    PharmacokineticModel,
+    Variable,
     CombinedModel,
-
+    Project,
+    Compound,
+    Dataset,
+    Protocol,
+    Subject,
+    SubjectGroup,
+    Unit
 )
 from django.contrib.auth.models import User
 
@@ -19,7 +27,29 @@ from rest_framework.test import APITestCase, APIClient
 
 class TestSimulateView(APITestCase):
     def setUp(self):
+        au = Unit.objects.get(symbol='mg')
+        tu = Unit.objects.get(symbol='h')
+        compound = Compound.objects.create(name="demo", compound_type="LM")
+        self.project = Project.objects.create(name='test project', compound=compound)
+        self.dataset = Dataset.objects.create(name='test dataset', project=self.project)
+        self.protocol = Protocol.objects.create(
+            name='my_cool_protocol',
+            compound=compound,
+            amount_unit=au,
+            time_unit=tu,
+            mapped_qname='PKCompartment.A1'
+        )
+        self.subject_group = SubjectGroup.objects.create(
+            name='my_cool_group',
+        )
+        self.subject = Subject.objects.create(
+            id_in_dataset=1,
+            dataset=self.dataset,
+            group=self.subject_group,
+            protocol=self.protocol,
+        )
         self.user = User.objects.create_user(username='testuser', password='12345')
+        self.project.users.add(self.user)
         self.client = APIClient()
         self.client.force_authenticate(user=self.user)
 
@@ -28,9 +58,14 @@ class TestSimulateView(APITestCase):
             name='tumour_growth_gompertz',
             read_only=False,
         )
+        pk = PharmacokineticModel.objects.get(
+            name="one_compartment_clinical",
+        )
         m = CombinedModel.objects.create(
             name='my wonderful model',
             pd_model=pd,
+            pk_model=pk,
+            project=self.project,
         )
 
         url = reverse('simulate-combined-model', args=(m.pk,))
@@ -44,6 +79,7 @@ class TestSimulateView(APITestCase):
         response = self.client.post(url, data, format='json')
 
         self.assertEqual(response.status_code, status.HTTP_200_OK)
+
         for sim in response.data:
             outputs = sim.get('outputs')
             self.assertCountEqual(
