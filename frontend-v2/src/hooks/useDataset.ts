5418315c52abcbe8d983648d566e0bbcffbb4733
Jim O'Donnell <james.odonnell@dtc.ox.ac.uk>
2024-04-08 21:03:15 +0100

    refactor: simplify data fetching
    Replace `useEffect` with event handlers for adding new datasets and finishing uploads.
    Refetch datasets rather than storing them in a global variable, so that the database is always the source of truth.

--- frontend-v2/src/hooks/useDataset.ts
+++ frontend-v2/src/hooks/useDataset.ts
@@ -1,4 +1,4 @@
-import { useCallback, useEffect, useState } from 'react';
+import { useCallback, useEffect } from 'react';
 import {
   DatasetRead,
   useDatasetListQuery,
@@ -9,11 +9,7 @@ import {
   useBiomarkerTypeListQuery
 } from '../app/backendApi';
 
-// assume only one dataset per project for the time being.
-let appDataset: DatasetRead | null = null;
-
 export default function useDataset(selectedProject: number | null) {
-  const [dataset, setDataset] = useState<null | DatasetRead>(appDataset);
   const selectedProjectOrZero = selectedProject || 0;
   const { data: project } =
     useProjectRetrieveQuery({ id: selectedProjectOrZero }, { skip: !selectedProject }
@@ -22,10 +18,11 @@ export default function useDataset(selectedProject: number | null) {
     { compoundId: project?.compound || 0 },
     { skip: !project?.compound },
   );
-  const { data: datasets = [], isLoading: isDatasetLoading } = useDatasetListQuery(
+  const { data: datasets = [], refetch } = useDatasetListQuery(
     { projectId: selectedProjectOrZero },
     { skip: !selectedProject },
   );
+  const [dataset] = datasets;
   const { data: subjectGroups, refetch: refetchSubjectGroups } = useSubjectGroupListQuery(
     { datasetId: dataset?.id || 0 },
     { skip: !dataset }
@@ -39,43 +36,30 @@ export default function useDataset(selectedProject: number | null) {
     createDataset
   ] = useDatasetCreateMutation();
 
-  if (dataset !== appDataset) {
-    setDataset(appDataset);
-  }
-
   useEffect(() => {
     if (dataset?.id) {
+      console.log('refetching groups and observations')
       refetchSubjectGroups();
       refetchBiomarkerTypes();
     }
   }, [dataset, refetchBiomarkerTypes, refetchSubjectGroups]);
 
-  useEffect(function onDataLoad() {
-    async function addDataset() {
-      let [newDataset] = datasets;
-      if (!newDataset) {
-        const response = await createDataset({
-          dataset: {
-            name: 'New Dataset',
-            project: selectedProjectOrZero,
-          }
-        });
-        if ('data' in response && response.data) {
-          newDataset = response.data;
-        }
+  
+  async function addDataset(projectId: number) {
+    const response = await createDataset({
+      dataset: {
+        name: 'New Dataset',
+        project: projectId,
       }
-      appDataset = newDataset;
-      setDataset(appDataset);
-    }
-    if (selectedProjectOrZero && !isDatasetLoading) {
-      addDataset();
-    }
-  }, [datasets, createDataset, isDatasetLoading, selectedProjectOrZero]);
+    });
+    console.log('created a new dataset')
+    return response;
+  }
 
   const updateDataset = useCallback((newDataset: DatasetRead) => {
-    appDataset = newDataset;
-    setDataset(appDataset);
-  }, []);
+    console.log('updating dataset', newDataset)
+    refetch();
+  }, [refetch]);
 
   const subjectBiomarkers = biomarkerTypes?.filter(b => b.is_continuous)
     .map(b => {
@@ -101,6 +85,7 @@ export default function useDataset(selectedProject: number | null) {
     dataset,
     groups: subjectGroups || [],
     subjectBiomarkers: subjectBiomarkers || [],
+    addDataset,
     updateDataset
   };
 }
