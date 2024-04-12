8c14117ae8c4433e2b6d23b0ca5e7b92a440992e
Jim O'Donnell <james.odonnell@dtc.ox.ac.uk>
2024-04-12 10:53:37 +0100

    feat: download a dataset as a CSV
    Add a download option to the CSV preview step, before uploading a CSV and converting it to a dataset.

--- frontend-v2/src/features/data/PreviewData.tsx
+++ frontend-v2/src/features/data/PreviewData.tsx
@@ -1,5 +1,5 @@
 import { FC } from 'react';
-import { Box, Table, TableHead, TableRow, TableCell, TableBody, Typography } from "@mui/material";
+import { Box, Button, Table, TableHead, TableRow, TableCell, TableBody, Typography } from "@mui/material";
 import { StepperState } from "./LoadDataStepper";
 
 interface IPreviewData {
@@ -10,11 +10,17 @@ interface IPreviewData {
 const PreviewData: FC<IPreviewData> = ({ state, firstTime }: IPreviewData) => {
   const { data } = state;
   const fields = [
-    ...state.fields,
-    'Group ID',
-    'Amount Variable',
-    'Observation Variable'
+    ...state.fields
   ];
+  if (!state.fields.find(field => field === 'Group ID')) {
+    fields.push('Group ID')
+  }
+  if (!state.fields.find(field => field === 'Amount Variable')) {
+    fields.push('Amount Variable')
+  }
+  if (!state.fields.find(field => field === 'Observation Variable')) {
+    fields.push('Observation Variable')
+  }
   if (!state.normalisedFields.find(field => field === 'Amount')) {
     fields.push('Amount')
   }
@@ -24,35 +30,61 @@ const PreviewData: FC<IPreviewData> = ({ state, firstTime }: IPreviewData) => {
   if (!state.normalisedFields.find(field => field === 'Amount Unit')) {
     fields.push('Amt_unit')
   }
-  if (!state.normalisedFields.find(field => field === 'Observation Unit')) {
+  if (
+    !state.normalisedFields.find(field => field === 'Observation Unit') &&
+    !state.fields.find(field => field === 'Observation_unit')
+  ) {
     fields.push('Observation_unit')
   }
 
+  function downloadCSV() {
+    const csvHeaders = fields.join(',');
+    const csvBody = data.map(row => fields.map(field => row[field]).join(',')).join('\n');
+    const csv = `${csvHeaders}\n${csvBody}`;
+    const blob = new Blob([csv], { type: 'text/csv' });
+    const url = window.URL.createObjectURL(blob);
+    const a = document.createElement('a');
+    a.href = url;
+    a.download = 'data.csv';
+    a.click();
+    window.URL.revokeObjectURL(url);
+  }
+
   return (
-    <Box component="div" sx={{ maxHeight: "40vh", overflow: 'auto', overflowX: 'auto' }}>
-      <Table>
-        <TableHead>
-          <TableRow>
-            {fields.map((field, index) => (
-              <TableCell key={index}>
-                <Typography variant="h6" component="div" sx={{ flexGrow: 1, marginBottom: 1 }} align="center">
-                  {field}
-                </Typography>
-              </TableCell>
-            ))}
-          </TableRow>
-        </TableHead>
-        <TableBody>
-          {data.map((row, index) => (
-            <TableRow key={index}>
+    <>
+      <Box component="div" sx={{ maxHeight: "40vh", overflow: 'auto', overflowX: 'auto' }}>
+        <Table>
+          <TableHead>
+            <TableRow>
               {fields.map((field, index) => (
-                <TableCell key={index}>{row[field]}</TableCell>
+                <TableCell key={index}>
+                  <Typography variant="h6" component="div" sx={{ flexGrow: 1, marginBottom: 1 }} align="center">
+                    {field}
+                  </Typography>
+                </TableCell>
               ))}
             </TableRow>
-          ))}
-        </TableBody>
-      </Table>
-    </Box>
+          </TableHead>
+          <TableBody>
+            {data.map((row, index) => (
+              <TableRow key={index}>
+                {fields.map((field, index) => (
+                  <TableCell key={index}>{row[field]}</TableCell>
+                ))}
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
+      </Box>
+      <Box sx={{ display: 'flex', justifyContent: 'right' }}>
+        <Button
+          variant="outlined"
+          onClick={downloadCSV}
+        >
+          Download CSV
+        </Button>
+      </Box>
+    </>
   )
 }
 
