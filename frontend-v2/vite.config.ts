c3a4bb828258357422fa4758c6578a7b42134851
Jim O'Donnell <james.odonnell@dtc.ox.ac.uk>
2024-06-04 17:35:42 +0100

    Split Plotly out of the vendor bundle

--- frontend-v2/vite.config.ts
+++ frontend-v2/vite.config.ts
@@ -4,6 +4,9 @@ import { defineConfig } from 'vite';
 import viteTsconfigPaths from 'vite-tsconfig-paths';
 
 function manualChunks(id) {
+  if (id.includes('node_modules/plotly.js')) {
+    return 'plotly';
+  }
   if (id.includes('node_modules')) {
     return 'vendor';
   }
