766d25c272b064c25cd14b6dbb675ed68790d58f
Jim O'Donnell <james.odonnell@dtc.ox.ac.uk>
2024-07-19 11:55:03 +0100

    fix(frontend): Vite Google Analytics config (#487)
    Use `loadEnv` to load the `.env` file dynamically at build time. Otherwise, `VITE_APP_GA_ID` isn't injected into the Docker build environment.

--- frontend-v2/vite.config.ts
+++ frontend-v2/vite.config.ts
@@ -1,6 +1,6 @@
 import react from "@vitejs/plugin-react";
 import svgr from "@svgr/rollup";
-import { defineConfig } from "vite";
+import { defineConfig, loadEnv } from "vite";
 import viteTsconfigPaths from "vite-tsconfig-paths";
 import { VitePluginRadar } from "vite-plugin-radar";
 
@@ -21,49 +21,51 @@ const proxy = {
   },
 };
 
-const { VITE_APP_GA_ID } = process.env;
-const radarOptions = {
-  enableDev: true,
-  analytics: {
-    id: VITE_APP_GA_ID,
-  },
-};
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [
-    react(),
-    viteTsconfigPaths(),
-    svgr(),
-    VitePluginRadar(radarOptions),
-  ],
-  build: {
-    outDir: "build",
-    rollupOptions: {
-      output: {
-        manualChunks: {
-          plotly: ["plotly.js-basic-dist-min"],
-          material: [
-            "@mui/material",
-            "@mui/icons-material",
-            "@mui/x-data-grid",
-          ],
-          vendor: [
-            "@reduxjs/toolkit",
-            "@reduxjs/toolkit/query",
-            "papaparse",
-            "react",
-            "react-dom",
-            "react-hook-form",
-            "react-dropzone",
-            "react-redux",
-            "react-toastify",
-          ],
+export default ({ mode }) => {
+  const { VITE_APP_GA_ID } = loadEnv(mode, process.cwd());
+  const radarOptions = {
+    enableDev: true,
+    analytics: {
+      id: VITE_APP_GA_ID,
+    },
+  };
+  return defineConfig({
+    plugins: [
+      react(),
+      viteTsconfigPaths(),
+      svgr(),
+      VitePluginRadar(radarOptions),
+    ],
+    build: {
+      outDir: "build",
+      rollupOptions: {
+        output: {
+          manualChunks: {
+            plotly: ["plotly.js-basic-dist-min"],
+            material: [
+              "@mui/material",
+              "@mui/icons-material",
+              "@mui/x-data-grid",
+            ],
+            vendor: [
+              "@reduxjs/toolkit",
+              "@reduxjs/toolkit/query",
+              "papaparse",
+              "react",
+              "react-dom",
+              "react-hook-form",
+              "react-dropzone",
+              "react-redux",
+              "react-toastify",
+            ],
+          },
         },
       },
     },
-  },
-  server: {
-    open: true,
-    proxy,
-  },
-});
+    server: {
+      open: true,
+      proxy,
+    },
+  });
+};
