b2b16df72eedb1f8094545013b289fdef0e19ffd
martinjrobins <martinjrobins@gmail.com>
2024-10-25 17:26:13 +0000

    add new models

--- pkpdapp/pkpdapp/migrations/0006_initial_pkpd_models.py
+++ pkpdapp/pkpdapp/migrations/0006_initial_pkpd_models.py
@@ -13,88 +13,6 @@ from pkpdapp.models import MyokitModelMixin
 
 
 def load_pkpd_models(apps, schema_editor):
-    models_pd = [
-        {
-            'name':
-            'tumour_growth_inhibition_model_koch',
-            'description':
-            '''
-Tumour growth inhibition pharmacodynamic model introduced by Koch et al. in
-[1].  In this model the tumour growth inhibition is modelled by an empirical
-model of the tumour volume \(V_T\) over time
-
-$$
-    \\frac{\\text{d}V_T}{\\text{d}t} =
-        \\frac{2\\lambda_0\\lambda_1 V_T}
-        {2\\lambda_0V_T + \\lambda_1} - \\kappa C V_T.
-$$
-
-Here, the tumour growth in absence of the drug is assumed to grow
-exponentially at rate \(2\\lambda_0\) for tumour volumes below some
-critical volume \(V_{\\text{crit}}\). For volumes beyond
-\(V_{\\text{crit}}\) the growth dynamics is assumed to slow down
-and transition to a linear growth at rate \(\\lambda_0\). The tumour
-growth inhibitory effect of the compound is modelled proportionally to
-its concentration \(C\) and the current tumour volume. The
-proportionality factor \(\\kappa\) can be interpreted as the potency
-of the drug.
-Note that the critical tumour volume \(V_{\\text{crit}}\) at which
-the growth dynamics transitions from exponential to linear growth is
-given by the two growth rates
-
-$$
-    V_{\\text{crit}} = \\frac{\\lambda _1}{2\\lambda _0}.
-$$
-
-**References**
-
-[1] Koch, G. et al. Modeling of tumor growth and anticancer effects
-    of combination therapy. J Pharmacokinet Pharmacodyn 36, 179–197
-    (2009).''',  # noqa: W605
-            'sbml_url':
-            'https://raw.githubusercontent.com/pkpdapp-team/pkpdapp-datafiles/main/models/tgi_Koch_2009.xml'  # noqa: E501
-        },
-        {
-            'name':
-            'tumour_growth_inhibition_model_koch_reparametrised',
-            'description':
-            """
-Returns the absolute path to a SBML file, specifying the tumour growth
-inhibition pharmacodynamic model introduced by Koch et al. in [1]_ with
-modified parametrisation.
-
-In this model the tumour growth inhibition is modelled by an empirical
-model of the tumour volume \(V_T\) over time
-
-$$
-    \\frac{\\text{d}V_T}{\\text{d}t} =
-        \\frac{\lambda V_T}
-        {V_T / V_{\\text{crit}} + 1} - \\kappa C V_T.
-$$
-
-Here, the tumour growth in absence of the drug is assumed to grow
-exponentially at rate \(\\lambda\) for tumour volumes below some
-critical volume \(V_{\\text{crit}}\). For volumes beyond
-\(V_{\\text{crit}}\) the growth dynamics is assumed to slow down
-and transition to a linear growth at rate
-\(\\lambda V_{\\text{crit}}\). The tumour growth inhibitory effect
-of the compound is modelled proportionally to its concentration
-\(C\) and the current tumour volume. The proportionality factor
-\(\\kappa\) can be interpreted as the potency of the drug.
-
-Note that this parameterisation of the model is related to the original
-parametersation in [1]_ by
-
-$$
-    V_{\\text{crit}} = \\frac{\\lambda _1}{2\\lambda _0} \\quad \\text{and}
-    \\quad \\lambda = 2\\lambda _1 .
-$$
-""",  # noqa: W605
-            'sbml_url':
-            'https://raw.githubusercontent.com/pkpdapp-team/pkpdapp-datafiles/main/models/tgi_Koch_2009_reparametrised.xml'  # noqa: E501
-        },
-    ]
-    models_pd = []
     models_pd_mmt = [
         {
             'name': 'indirect_effects_stimulation_elimination',
@@ -383,90 +301,48 @@ Description of a clinical two compartment TMDD model here.
             """,  # noqa: W605
             'mmt_filename': 'pkpdapp/migrations/models/TMDD_QSS_ConstTarget_2cmpt_PK_Model_preclinical.mmt'  # noqa: E501
         },
+        {
+            'name': 'three_compartment_mammillary',
+            'description':
+            """
+            3-compartment mammillary PK Model - CL implementation
+            """,  # noqa: W605
+            'mmt_filename': 'pkpdapp/migrations/models/Dis_Classic_3_Compartmental_Model_CL_mammilary.mmt'  # noqa: E501
+        },
+        {
+            'name': 
+            'absorption_first_order',
+            'description':
+            """
+First order absorption model, single absorption site (connects to any PK model)
+""",  # noqa: W605
+            'mmt_filename':
+            'pkpdapp/migrations/models/Abs_01_FirstOrder.mmt'  # noqa: E501
+        },
+        {
+            'name': 
+            'absorption_transit',
+            'description':
+            """
+Transit compartment absorption model (up to 10 transit compartments)
+""",  # noqa: W605
+            'mmt_filename':
+            'pkpdapp/migrations/models/Abs_02_Transit.mmt'  # noqa: E501
+        },
+        {
+            'name': 
+            'absorption_ophtha',
+            'description':
+            """
+Occular PK model including Vitreous and Aqueous Humour (can be connected to any systemic PK model)
+""",  # noqa: W605
+            'mmt_filename':
+            'pkpdapp/migrations/models/Abs_03_Ophtha_PK.mmt'  # noqa: E501
+        },
     ]
 
     PharmacodynamicModel = apps.get_model("pkpdapp", "PharmacodynamicModel")
     PharmacokineticModel = apps.get_model("pkpdapp", "PharmacokineticModel")
-    Unit = apps.get_model("pkpdapp", "Unit")
-    Variable = apps.get_model("pkpdapp", "Variable")
-    for m in models_pd:
-        try:
-            with urllib.request.urlopen(m['sbml_url'], timeout=5) as f:
-                sbml_string = codecs.decode(f.read(), 'utf-8')
-                model = PharmacodynamicModel.objects.create(
-                    name=m['name'],
-                    description=m['description'],
-                    mmt=MyokitModelMixin.sbml_string_to_mmt(sbml_string),
-                    is_library_model=True,
-                )
-
-                myokit_model = SBMLParser().parse_string(
-                    str.encode(sbml_string)
-                ).myokit_model()
-
-                for i, v in enumerate(myokit_model.variables()):
-                    unit = v.unit()
-                    exponents = unit.exponents()
-                    multiplier = unit.multiplier_log_10()
-                    close_enough = 1e-9
-                    close_enough_units = Unit.objects.filter(
-                        g__range=(
-                            exponents[0] - close_enough,
-                            exponents[0] + close_enough,
-                        ),
-                        m__range=(
-                            exponents[1] - close_enough,
-                            exponents[1] + close_enough,
-                        ),
-                        s__range=(
-                            exponents[2] - close_enough,
-                            exponents[2] + close_enough,
-                        ),
-                        A__range=(
-                            exponents[3] - close_enough,
-                            exponents[3] + close_enough,
-                        ),
-                        K__range=(
-                            exponents[4] - close_enough,
-                            exponents[4] + close_enough,
-                        ),
-                        cd__range=(
-                            exponents[5] - close_enough,
-                            exponents[5] + close_enough,
-                        ),
-                        mol__range=(
-                            exponents[6] - close_enough,
-                            exponents[6] + close_enough,
-                        ),
-                        multiplier__range=(
-                            multiplier - close_enough,
-                            multiplier + close_enough,
-                        ),
-                    )
-                    if close_enough_units.count() > 0:
-                        stored_unit = close_enough_units[0]
-                    else:
-                        raise RuntimeError(
-                            'Unit {} {} ({}) does not exist'.format(
-                                unit, unit.exponents(),
-                                unit.multiplier_log_10()
-                            )
-                        )
-
-                    Variable.objects.create(
-                        name=v.name(),
-                        qname=v.qname(),
-                        constant=v.is_constant(),
-                        state=v.is_state(),
-                        binding=v.binding(),
-                        unit=stored_unit,
-                        pd_model=model,
-                        color=i,
-                        display=v.name() != 'time',
-                    )
-
-        except urllib.error.URLError:
-            print('WARNING: urlopen timed-out, no data loaded')
 
     for m in models_pd_mmt:
         try:
