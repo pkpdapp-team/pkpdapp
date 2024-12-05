690e02ff199d99757b58d88459d250954daf0379
martinjrobins <martinjrobins@gmail.com>
2024-12-05 11:22:43 +0000

    add new models and tag model

---
+++ pkpdapp/pkpdapp/models/tag.py
@@ -0,0 +1,11 @@
+# django model for finite list of tags (use an enum field)
+
+from django.db import models
+
+
+class Tag(models.Model):
+    """
+    A tag for a model
+    """
+
+    name = models.CharField(max_length=10, help_text="name of the tag")
