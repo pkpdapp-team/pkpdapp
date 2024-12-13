fb0f39a928f104b2a066c40405b3b78eae05cfa9
martinjrobins <martinjrobins@gmail.com>
2024-12-13 15:43:57 +0000

    tag filtering

---
+++ pkpdapp/pkpdapp/api/serializers/tag.py
@@ -0,0 +1,13 @@
+# This file is part of PKPDApp (https://github.com/pkpdapp-team/pkpdapp) which
+# is released under the BSD 3-clause license. See accompanying LICENSE.md for
+# copyright notice and full license details.
+#
+
+from rest_framework import serializers
+from pkpdapp.models import Tag
+
+
+class TagSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Tag
+        fields = "__all__"
