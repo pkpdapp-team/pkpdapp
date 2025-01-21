d146a62d8e3429ea3386c4dfe924735ee35075c6
Jim O'Donnell <james.odonnell@dtc.ox.ac.uk>
2025-01-21 11:30:51 +0000

    ci: ignore API client in ESLint config (#645)

--- frontend-v2/eslint.config.js
+++ frontend-v2/eslint.config.js
@@ -1,10 +1,10 @@
-import js from '@eslint/js';
-import prettier from 'eslint-config-prettier';
-import reactRecommended from 'eslint-plugin-react/configs/recommended.js';
-import reactJSXRuntime from 'eslint-plugin-react/configs/jsx-runtime.js';
-import reactHooks from 'eslint-plugin-react-hooks';
-import typescript from 'typescript-eslint';
-import pluginJsxA11y from 'eslint-plugin-jsx-a11y';
+import js from "@eslint/js";
+import prettier from "eslint-config-prettier";
+import reactRecommended from "eslint-plugin-react/configs/recommended.js";
+import reactJSXRuntime from "eslint-plugin-react/configs/jsx-runtime.js";
+import reactHooks from "eslint-plugin-react-hooks";
+import typescript from "typescript-eslint";
+import pluginJsxA11y from "eslint-plugin-jsx-a11y";
 
 export default [
   js.configs.recommended,
@@ -14,14 +14,17 @@ export default [
   prettier,
   {
     plugins: {
-      'jsx-a11y': pluginJsxA11y,
-      'react-hooks': reactHooks,
+      "jsx-a11y": pluginJsxA11y,
+      "react-hooks": reactHooks,
     },
     rules: pluginJsxA11y.configs.recommended.rules,
     settings: {
       react: {
-        version: "detect"
-      }
-    }
-  }
-]
+        version: "detect",
+      },
+    },
+  },
+  {
+    ignores: ["src/app/api.ts", "src/app/backendApi.ts"],
+  },
+];
