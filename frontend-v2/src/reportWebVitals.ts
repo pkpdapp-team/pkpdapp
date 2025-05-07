45fae8fee9ac662ae2004df6366d0fb61f3fa1c2
Jim O'Donnell <james.odonnell@dtc.ox.ac.uk>
2025-05-07 12:06:05 +0100

    build(frontend): update dependencies
    - major version updates for dev dependencies.
    - use `yarn build` to test the build in CI.
    - update the frontend OpenAPI client.
    - track core web vitals in the console.

--- frontend-v2/src/reportWebVitals.ts
+++ frontend-v2/src/reportWebVitals.ts
@@ -1,14 +1,12 @@
-import { ReportHandler } from "web-vitals";
+import { onCLS, onFID, onFCP, onLCP, onTTFB } from "web-vitals";
 
-const reportWebVitals = (onPerfEntry?: ReportHandler) => {
+const reportWebVitals = (onPerfEntry = console.log) => {
   if (onPerfEntry && onPerfEntry instanceof Function) {
-    import("web-vitals").then(({ getCLS, getFID, getFCP, getLCP, getTTFB }) => {
-      getCLS(onPerfEntry);
-      getFID(onPerfEntry);
-      getFCP(onPerfEntry);
-      getLCP(onPerfEntry);
-      getTTFB(onPerfEntry);
-    });
+    onCLS(onPerfEntry);
+    onFID(onPerfEntry);
+    onFCP(onPerfEntry);
+    onLCP(onPerfEntry);
+    onTTFB(onPerfEntry);
   }
 };
 
