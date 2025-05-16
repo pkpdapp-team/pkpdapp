334b1164f2cfae49e01dc4ad60028527ab00d207
Jim O'Donnell <james.odonnell@dtc.ox.ac.uk>
2025-05-16 15:20:53 +0100

    simplify the state logic

--- frontend-v2/src/app/hooks.ts
+++ frontend-v2/src/app/hooks.ts
@@ -47,12 +47,7 @@ export function useFieldState<T extends FieldValues>({
   const [fieldValue, setFieldValue] = useState<any>(initialValue);
 
   useEffect(() => {
-    setFieldValue((value: typeof initialValue) => {
-      if (value === initialValue) {
-        return value;
-      }
-      return initialValue;
-    });
+    setFieldValue(initialValue);
   }, [initialValue]);
 
   return [fieldValue, setFieldValue];
