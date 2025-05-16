df175c0310747be4ffc548fe665fbd458b8e75cc
Jim O'Donnell <james.odonnell@dtc.ox.ac.uk>
2025-05-16 10:12:58 +0100

    split large hooks into smaller functions

--- frontend-v2/src/features/model/Model.tsx
+++ frontend-v2/src/features/model/Model.tsx
@@ -100,14 +100,16 @@ function useApiQueries() {
   };
 }
 
-function useModelFormState({
+function useModelFormDataCallback({
   model,
   project,
   simulation,
+  reset,
 }: {
   model?: CombinedModelRead | null;
   project?: ProjectRead;
   simulation?: SimulationRead;
+  reset: (values?: Partial<FormData>) => void;
 }) {
   const [updateModel] = useCombinedModelUpdateMutation();
   const [updateSimulation] = useSimulationUpdateMutation();
@@ -115,23 +117,6 @@ function useModelFormState({
 
   const [setParamsToDefault] =
     useCombinedModelSetParamsToDefaultsUpdateMutation();
-
-  const species = project?.species;
-  const defaultValues: FormData = {
-    ...DEFAULT_MODEL,
-    project: project?.id || 0,
-    species,
-  };
-  const values = getFormValues(model, species);
-  const { reset, handleSubmit, control } = useForm<FormData>({
-    defaultValues,
-    values,
-  });
-  const { isDirty, isSubmitting } = useFormState({
-    control,
-  });
-
-  useDirty(isDirty);
 
   const handleFormData = useCallback(
     (data: FormData) => {
@@ -197,15 +182,34 @@ function useModelFormState({
       reset,
     ],
   );
+  return handleFormData;
+}
 
-  useEffect(() => {
-    if (isDirty && !isSubmitting) {
-      const submit = handleSubmit(handleFormData);
-      submit();
-    }
-  }, [handleSubmit, handleFormData, isDirty, isSubmitting]);
+function useModelFormState({
+  model,
+  project,
+}: {
+  model?: CombinedModelRead | null;
+  project?: ProjectRead;
+}) {
+  const species = project?.species;
+  const defaultValues: FormData = {
+    ...DEFAULT_MODEL,
+    project: project?.id || 0,
+    species,
+  };
+  const values = getFormValues(model, species);
+  const { reset, handleSubmit, control } = useForm<FormData>({
+    defaultValues,
+    values,
+  });
+  const { isDirty } = useFormState({
+    control,
+  });
 
-  return { control };
+  useDirty(isDirty);
+
+  return { control, reset, handleSubmit };
 }
 
 const DEFAULT_MODEL: CombinedModelRead = {
@@ -253,7 +257,30 @@ const Model: FC = () => {
     variables,
   } = useApiQueries();
 
-  const { control } = useModelFormState({ model, project, simulation });
+  const { control, reset, handleSubmit } = useModelFormState({
+    model,
+    project,
+  });
+  const handleFormData = useModelFormDataCallback({
+    model,
+    project,
+    simulation,
+    reset,
+  });
+
+  /* 
+  Submit whenever the form is dirty
+  and previous updates have finished (or reset).
+  */
+  const { isDirty, isSubmitting } = useFormState({
+    control,
+  });
+  useEffect(() => {
+    if (isDirty && !isSubmitting) {
+      const submit = handleSubmit(handleFormData);
+      submit();
+    }
+  }, [handleSubmit, handleFormData, isDirty, isSubmitting]);
 
   if (isLoading) {
     return <div>Loading...</div>;
