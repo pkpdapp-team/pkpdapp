e876bca04b595b80126709cdbe4b4c69dd2589a2
Jim O'Donnell <james.odonnell@dtc.ox.ac.uk>
2025-11-06 15:00:09 +0000

    fix(frontend): duplicate variable names in Results
    Display effect compartment variables as Ce1, Ce2, Ce3… in the Results view.

--- frontend-v2/src/features/results/useConcentrationVariables.ts
+++ frontend-v2/src/features/results/useConcentrationVariables.ts
@@ -5,6 +5,7 @@ import { SimulationContext } from "../../contexts/SimulationContext";
 import { RootState } from "../../app/store";
 import { useCombinedModelListQuery } from "../../app/backendApi";
 import { useVariables } from "./useVariables";
+import { renameVariable } from "../simulation/Simulations";
 
 export function useConcentrationVariables() {
   const { simulations } = useContext(SimulationContext);
@@ -16,18 +17,20 @@ export function useConcentrationVariables() {
     { projectId: projectIdOrZero },
     { skip: !projectId },
   );
-  const model = models?.[0] || null;
+  const model = models?.[0];
   const variables = useVariables();
   const simulationVariableIDs = simulations?.[0]?.outputs
     ? Object.keys(simulations[0].outputs).map((key) => parseInt(key))
     : [];
   return (
-    variables?.filter(
-      (variable) =>
-        simulationVariableIDs.includes(variable.id) &&
-        model?.derived_variables?.find(
-          (dv) => dv.pk_variable === variable.id && dv.type === "AUC",
-        ),
-    ) || []
+    variables
+      ?.filter(
+        (variable) =>
+          simulationVariableIDs.includes(variable.id) &&
+          model?.derived_variables?.find(
+            (dv) => dv.pk_variable === variable.id && dv.type === "AUC",
+          ),
+      )
+      .map((v) => renameVariable(v, model)) || []
   );
 }
