05fe71f3fa8fce307d0f8a3d07868d17f3408a5b
Jim O'Donnell <james.odonnell@dtc.ox.ac.uk>
2026-01-23 18:13:04 +0000

    add a test

--- frontend-v2/src/stories/Simulation.stories.tsx
+++ frontend-v2/src/stories/Simulation.stories.tsx
@@ -190,6 +190,23 @@ export const AddNewPlot: Story = {
   },
 };
 
+export const AddNewConcentrationPlot: Story = {
+  play: async ({ canvasElement, userEvent }) => {
+    const canvas = within(canvasElement);
+    const addPlotButton = await canvas.findByRole("button", {
+      name: "Add new plot",
+    });
+    expect(addPlotButton).toBeInTheDocument();
+    await userEvent.click(addPlotButton);
+
+    const variableButton = await screen.findByRole("button", {
+      name: /^C1/,
+    });
+    expect(variableButton).toBeInTheDocument();
+    await userEvent.click(variableButton);
+  },
+};
+
 export const EditPlot: Story = {
   play: async ({ canvasElement, userEvent }) => {
     // Wait for Plotly to load the plots.
