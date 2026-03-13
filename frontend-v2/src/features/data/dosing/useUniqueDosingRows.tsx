763527084ee1eadf21fbb3439b430c1f47141b2c
Jim O'Donnell <james.odonnell@dtc.ox.ac.uk>
2026-03-13 18:11:23 +0000

    fix: allow for multiple doses in uploaded data
    Allow for cases where each admin ID/group in an uploaded CSV may have more than one dose amount, doesed at different times. Add a test case to the storybook and update test assertions to reflect the increased number of dosing protocol rows.

---
+++ frontend-v2/src/features/data/dosing/useUniqueDosingRows.tsx
@@ -0,0 +1,24 @@
+import { useRef } from "react";
+import { Row } from "../LoadData";
+
+export function useUniqueDosingRows(
+  dosingRows: Row[],
+  doseGroupingFields: string[],
+  dosingRowKeyField: string,
+): Row[] {
+  const nextDosingRowKey = useRef(0);
+  const uniqueDosingRows = dosingRows.filter(
+    (row, index, arr) =>
+      doseGroupingFields.length === 0 ||
+      arr.findIndex((r) =>
+        doseGroupingFields.every((field) => r[field] === row[field]),
+      ) === index,
+  );
+  uniqueDosingRows.forEach((row) => {
+    if (!row[dosingRowKeyField]) {
+      row[dosingRowKeyField] = `dosing-row-${nextDosingRowKey.current}`;
+      nextDosingRowKey.current += 1;
+    }
+  });
+  return uniqueDosingRows;
+}
