c68568588253fa2a7e22f54968746bce395d44df
Martin Robinson <martinjrobins@gmail.com>
2026-06-16 15:19:17 +0100

    feat: remove old update models/units from start-server

--- start-server.sh
+++ start-server.sh
@@ -1,8 +1,6 @@
 #!/usr/bin/env bash
 # start-server.sh
 (python manage.py migrate --no-input)
-(python update_models.py)
-(python update_units.py)
 (memcached -l 127.0.0.1 -p 11211 -m "${MEMCACHED_MEMORY_MB:-64}" -U 0) &
 (gunicorn pkpdapp.wsgi:application --bind unix:/run/gunicorn.socket --workers 3) &
 nginx -g "daemon off;"
