9d2601ee61561f951ba9b3cd46589e711e7f6a19
martinjrobins <martinjrobins@gmail.com>
2026-07-15 18:10:10 +0000

    fix: update models migration

--- pkpdapp/pkpdapp/migrations/0066_update_models.py
+++ pkpdapp/pkpdapp/migrations/0066_update_models.py
@@ -5,11 +5,17 @@
 #
 
 from django.db import migrations
+import importlib
+
+
+v3models = importlib.import_module("pkpdapp.migrations.0027_version_3_models")
 
 
 class Migration(migrations.Migration):
     dependencies = [
-        ("pkpdapp", "0059_tgi_dimensionless_units"),
+        ("pkpdapp", "0065_alter_derivedvariable_type"),
     ]
 
-    operations = []
+    operations = [
+        migrations.RunPython(v3models.load_pkpd_models),
+    ]
